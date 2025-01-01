dc05033e812d7959c246dba68b24a564a0042e52
AshGw <ashrefgw@proton.me>
2025-01-01 11:56:25 +0100

    fix: async next config problem

--- next.config.ts
+++ next.config.ts
@@ -82,7 +82,9 @@ const nextConfig: NextConfig = {
 };
 
 if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
+  (async () => {
+    await setupDevPlatform();
+  })();
 }
 
 export default nextConfig;
