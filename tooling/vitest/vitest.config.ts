9d002e4bc37f3bf061300d19ed83f704acf61e69
AshGw <ashrefgw@proton.me>
2025-01-11 13:35:21 +0100

    chore: add vitest workspace file

--- tooling/vitest/vitest.config.ts
+++ tooling/vitest/vitest.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom", // or 'node' based on your needs
-    setupFiles: "./setup.ts", // Optional: path to your setup file
     coverage: {
       reporter: ["text", "json", "html"],
     },
---
+++ vitest.workspace.ts
@@ -0,0 +1,4 @@
+export default [
+  "apps/*/vitest.config.{e2e,unit,integration}.ts",
+  "packages/*/vitest.config.ts",
+];
