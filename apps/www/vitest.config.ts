e796936d47e23d52f935714e0a992d269c915cdf
AshGw <ashrefgw@proton.me>
2025-01-11 15:44:54 +0100

    fix: stub module res

--- apps/www/vitest.config.ts
+++ apps/www/vitest.config.ts
@@ -1,6 +1,8 @@
-import { baseConfig } from '@ashgw/vitest-config';
 import path from 'path';
 import { defineProject, mergeConfig } from 'vitest/config';
+
+// semehow @ashgw/vitest-config keep erroring out, and it's annoying af!
+import { baseConfig } from './../../tooling/vitest';
 
 export default mergeConfig(
   baseConfig,
