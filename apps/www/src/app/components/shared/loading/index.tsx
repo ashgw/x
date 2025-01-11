be3cf2062e749f48b32d35e0920f5b5a101480e3
AshGw <ashrefgw@proton.me>
2025-01-11 19:21:46 +0100

    build: update the `loading-points` version

--- apps/www/src/app/components/shared/loading/index.tsx
+++ apps/www/src/app/components/shared/loading/index.tsx
@@ -1,11 +1,7 @@
 'use client';
 
 import LoadingPoints from 'loading-points';
-import type { Pixel, RGB } from 'loading-points';
 
 export function Loading() {
-  const glowColor = 'rgb(155, 46, 199)' as RGB;
-  const circleSize = '8px' as Pixel;
-
-  return <LoadingPoints glowColor={glowColor} circleSize={circleSize} />;
+  return <LoadingPoints glowColor={'rgb(155, 46, 199)'} circleSize={'8px'} />;
 }
