1e5ff81df9f11ab97e935cf2c7aae3f0b0ddf44b
AshGw <ashrefgw@proton.me>
2025-01-12 11:21:38 +0100

    fix: regex matchers

--- apps/blog/src/lib/services/Names.service.ts
+++ apps/blog/src/lib/services/Names.service.ts
@@ -1,17 +1,14 @@
-// TODO: fix ts-roids
-// import { Optional } from 'ts-roids';
 type Optional<T> = T | null;
 export const URL_REGEX = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}$/; // eslint-disable-line no-useless-escape
 export const OPTIONAL_PROTOCOL_URL_REGEX =
   /^(?:https?:\/\/)?(?:www\.)?([^\/]+)(?:\/.*)?$/; // eslint-disable-line no-useless-escape
 
 export class NamesService {
-  public static matchURL(dto: { url: string }): Optional<RegExpMatchArray> {
-    /* eslint-disable @typescript-eslint/prefer-regexp-exec */
-    return dto.url.match(URL_REGEX);
+  public static matchURL(dto: { url: string }): Optional<RegExpExecArray> {
+    return URL_REGEX.exec(dto.url);
   }
   public static getSiteName({ url }: { url: string }): Optional<string> {
-    const match = url.match(OPTIONAL_PROTOCOL_URL_REGEX);
+    const match = OPTIONAL_PROTOCOL_URL_REGEX.exec(url);
     if (match) {
       const parts = match[1]?.split(".");
       if (!parts) {
