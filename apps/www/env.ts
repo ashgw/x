005aa046e7b41645439621f642d71d719c8fd7fd
AshGw <ashrefgw@proton.me>
2025-01-12 21:31:19 +0100

    chore: clearup constants

---
+++ apps/www/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from '@ashgw/ts-env';
+import { z } from 'zod';
+
+export const env = createEnv({
+  vars: {
+    NODE_ENV: z.enum(['production', 'development', 'staging']),
+    WWW_URL: z.string().url(),
+    BLOG_URL: z.string().url(),
+    CONTACT_EMAIL: z.string().email(),
+    GENERATE_SOURCE_MAP: z.boolean(),
+    GA_ID: z.string().min(1),
+  },
+  disablePrefix: ['NODE_ENV'],
+  prefix: 'NEXT_PUBLIC',
+  skipValidation: false,
+});
