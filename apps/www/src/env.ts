b05e7d99a09983033f80eec79d72303bd03d18ae
AshGw <ashrefgw@proton.me>
2025-01-12 23:36:46 +0100

    build: create the env package

--- apps/www/src/env.ts
+++ apps/www/src/env.ts
@@ -1,17 +1,3 @@
-import { createEnv } from '@ashgw/ts-env';
-import { config } from 'dotenv';
-import { z } from 'zod';
+import { env as repoEnv } from '@ashgw/env';
 
-config({ path: require('path').resolve(__dirname, '../../../.env') });
-
-export const env = createEnv({
-  vars: {
-    NODE_ENV: z.enum(['production', 'development', 'staging']),
-    WWW_URL: z.string().url(),
-    WWW_GOOGLE_ANALYTICS_ID: z.string().min(7).startsWith('G'),
-    BLOG_URL: z.string().url(),
-  },
-  disablePrefix: ['NODE_ENV'],
-  prefix: 'NEXT_PUBLIC',
-  skipValidation: false,
-});
+export const env = repoEnv;
---
+++ packages/env/index.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import { z } from "zod";
+
+import { createEnv } from "@ashgw/ts-env";
+
+config({ path: require("path").resolve(__dirname, "../../.env") });
+
+export const env = createEnv({
+  vars: {
+    NODE_ENV: z.enum(["production", "development", "staging"]),
+    WWW_URL: z.string().url(),
+    WWW_GOOGLE_ANALYTICS_ID: z.string().min(7).startsWith("G"),
+    BLOG_GOOGLE_ANALYTICS_ID: z.string().min(7).startsWith("G"),
+    BLOG_URL: z.string().url(),
+  },
+  disablePrefix: ["NODE_ENV"],
+  prefix: "NEXT_PUBLIC",
+  skipValidation: false,
+});
