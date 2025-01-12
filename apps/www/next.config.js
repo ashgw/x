849222c60bb4ca72ab076d71bebf6b2d572e0cef
AshGw <ashrefgw@proton.me>
2025-01-12 21:59:34 +0100

    test: build

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -2,11 +2,7 @@ import baseConfig from '@ashgw/next-config/base.js';
 import { createJiti } from 'jiti';
 import { fileURLToPath } from 'url';
 
-/* 
-  @see https://github.com/t3-oss/create-t3-turbo/blob/main/apps/nextjs/next.config.js
-  for env vars validation
-*/
-// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+// Import the env file to validate at build time. Use jiti so we can load .ts files in here.
 await createJiti(fileURLToPath(import.meta.url)).import('./src/env');
 
 /** @type {import("next").NextConfig} */
