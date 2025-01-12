3518f4cf9a078938954c5fcddb4049f81d23cf45
AshGw <ashrefgw@proton.me>
2025-01-12 17:35:14 +0100

    chore: add presets

--- tooling/tailwind/web.ts
+++ tooling/tailwind/web.ts
@@ -4,8 +4,7 @@ import animate from "tailwindcss-animate";
 import { baseConfig } from "./base";
 
 export const webConfig = {
-  content: baseConfig.content,
-  presets: [baseConfig],
+  ...baseConfig,
   theme: {
     container: {
       center: true,
@@ -15,6 +14,41 @@ export const webConfig = {
       },
     },
     extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
