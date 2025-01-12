18408c389be9e5fd47cd61ad07e2f7451660e105
AshGw <ashrefgw@proton.me>
2025-01-12 16:36:12 +0100

    fix: resolve UI issue

--- apps/blog/tailwind.config.ts
+++ apps/blog/tailwind.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
-import { baseConfig } from "@ashgw/tailwind-config";
+import { webConfig } from "@ashgw/tailwind-config";
 
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
-  content: [...baseConfig.content, "../../packages/ui/src/*.{ts,tsx}"],
-  presets: [baseConfig],
+  content: [...webConfig.content, "../../packages/ui/src/*.{ts,tsx}"],
+  presets: [webConfig],
   theme: {
     extend: {
       fontFamily: {
