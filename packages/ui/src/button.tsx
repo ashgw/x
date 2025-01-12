925a7f4dddd134c16780593af1003ac2d1762d08
AshGw <ashrefgw@proton.me>
2025-01-12 16:04:03 +0100

    fix: formatting

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,21 +6,21 @@ import { cva } from "class-variance-authority";
 import { cn } from "@ashgw/ui";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         navbar:
-          "glowsup hover:average-translate rounded-[1.1rem] bg-primary/95 font-semibold text-primary-foreground opacity-95 transition-all hover:bg-primary hover:opacity-100",
+          "glowsup hover:average-translate bg-primary/95 text-primary-foreground hover:bg-primary rounded-[1.1rem] font-semibold opacity-95 transition-all hover:opacity-100",
         navbarMin:
-          "glowsup average-transition rounded-[1.1rem] bg-primary/95 text-primary-foreground opacity-95 hover:bg-primary hover:opacity-100",
+          "glowsup average-transition bg-primary/95 text-primary-foreground hover:bg-primary rounded-[1.1rem] opacity-95 hover:opacity-100",
 
         default:
-          "average-transition rounded-[1.1rem] bg-primary/95 text-primary-foreground opacity-95 hover:bg-primary hover:opacity-100",
+          "average-transition bg-primary/95 text-primary-foreground hover:bg-primary rounded-[1.1rem] opacity-95 hover:opacity-100",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "nav-link-shadow rounded-[1.1rem] border border-input bg-transparent hover:text-accent-foreground",
+          "nav-link-shadow border-input hover:text-accent-foreground rounded-[1.1rem] border bg-transparent",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
