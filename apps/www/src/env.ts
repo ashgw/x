b2f46420737a41e1bb3d36bbe7075eeeda4559e0
AshGw <ashrefgw@proton.me>
2025-01-12 22:06:21 +0100

    fix: load env vars correctly

--- apps/www/src/env.ts
+++ apps/www/src/env.ts
@@ -1,12 +1,14 @@
 import { createEnv } from '@ashgw/ts-env';
+import { config } from 'dotenv';
 import { z } from 'zod';
+
+config({ path: require('path').resolve(__dirname, '../../../.env') });
 
 export const env = createEnv({
   vars: {
     NODE_ENV: z.enum(['production', 'development', 'staging']),
     WWW_URL: z.string().url(),
     BLOG_URL: z.string().url(),
-    GENERATE_SOURCE_MAP: z.boolean(),
     GA_ID: z.string().min(1),
   },
   disablePrefix: ['NODE_ENV'],
