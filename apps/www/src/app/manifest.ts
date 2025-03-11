ef15b7191a0bf22a0a87fdbf15b345ce7d47203f
ashgw <ashrefgw@proton.me>
2025-03-11 01:07:38 +0100

    chore: update manifest files

--- apps/www/src/app/manifest.ts
+++ apps/www/src/app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Ashgw",
-    short_name: "Ashgw",
-    description: "AG's corner on the internet",
+    name: "@ashgw",
+    short_name: "ashgw",
+    description: "AG's personal site",
     start_url: "/",
     display: "standalone",
     background_color: "#21001d",
