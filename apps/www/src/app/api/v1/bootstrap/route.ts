c28e201c5c8d3159a929eed0c1c7527e9703e37c
ashgw <ashrefgw@proton.me>
2025-03-21 22:44:05 +0100

    feat: finish linux setup bootsrap script

--- apps/www/src/app/api/v1/bootstrap/route.ts
+++ apps/www/src/app/api/v1/bootstrap/route.ts
@@ -1,21 +1,21 @@
-import { NextResponse } from 'next/server';
-import { NewType } from 'ts-roids';
+import type { NewType } from "ts-roids";
+import { NextResponse } from "next/server";
 
-// TODO: KEEP TRACK OF STABLE COMMITS, DON'T JUST FETCH OFF OF MAIN
-const BOOTSTRAP_SCRIPT =
-  'https://raw.githubusercontent.com/AshGw/dotfiles/main/install/bootstrap' as const;
-
-type Script = NewType<'Key', string>;
+type Script = NewType<"Key", string>;
 type Err = string;
+
 export async function GET(): Promise<NextResponse<Script> | NextResponse<Err>> {
   try {
-    const res = await fetch(BOOTSTRAP_SCRIPT, {
-      method: 'GET',
-      mode: 'no-cors',
-      next: { revalidate: 36969 },
-    });
+    const res = await fetch(
+      "https://raw.githubusercontent.com/AshGw/dotfiles/main/install/bootstrap",
+      {
+        method: "GET",
+        mode: "no-cors",
+        next: { revalidate: 36969 },
+      },
+    );
     if (!res.ok) {
-      return new NextResponse('Failed to fetch the bootstrap script', {
+      return new NextResponse("Failed to fetch the bootstrap script", {
         status: 424,
       });
     }
@@ -24,7 +24,7 @@ export async function GET(): Promise<NextResponse<Script> | NextResponse<Err>> {
     return new NextResponse(script, { status: 200 });
   } catch (e) {
     console.log(e);
-    return new NextResponse('Something unexpected happened', {
+    return new NextResponse("Something unexpected happened", {
       status: 500,
     });
   }
