c6338f8a0b1a50f074c8222721928d64b92e932b
ashgw <ashrefgw@proton.me>
2025-03-21 17:46:10 +0100

    chore: add `vercel` preset

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,23 +1,29 @@
 import path from "path";
+import type { UnionToTuple } from "ts-roids";
 import { config } from "dotenv";
 import { z } from "zod";
 
-import { createEnv } from "@ashgw/ts-env";
+import { createEnv, preset } from "@ashgw/ts-env";
 
 config({ path: path.resolve(__dirname, "../../.env") });
 
 const isBrowser = typeof window !== "undefined";
 
+const vercelPreset = preset("vercel");
+
 export const env = createEnv({
   vars: {
     NODE_ENV: z.enum(["production", "development", "preview"]),
     WWW_URL: z.string().url(),
+    BLOG_URL: z.string().url(),
     WWW_GOOGLE_ANALYTICS_ID: z.string().min(7).startsWith("G-"),
     BLOG_GOOGLE_ANALYTICS_ID: z.string().min(7).startsWith("G-"),
-    BLOG_URL: z.string().url(),
+    ...vercelPreset,
   },
-  disablePrefix: ["NODE_ENV"],
+  disablePrefix: [
+    "NODE_ENV",
+    ...(Object.keys(vercelPreset) as UnionToTuple<keyof typeof vercelPreset>),
+  ],
   prefix: "NEXT_PUBLIC",
-  skipValidation: isBrowser, // Skip validation in the browser to prevent runtime errors.
-  // Since env vars are already injected at build time, we don't need to validate them at runtime.
+  skipValidation: isBrowser, // Since env vars are already injected at build time, we don't need to validate them at runtime.
 });
