85cc56f8c96ec9e571083ea823db7bf9e74cfe23
ashgw <ashrefgw@proton.me>
2025-03-21 17:21:18 +0100

    feat: enhance opengraph image color variation

--- apps/www/src/app/opengraph-image.tsx
+++ apps/www/src/app/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 
-export const alt = "Ashgw";
+export const alt = "@ashgw Website";
 
 export const size = {
   width: 1200,
@@ -8,23 +8,31 @@ export const size = {
 };
 
 export const contentType = "image/png";
+
 export default function Image() {
   return new ImageResponse(
     (
       <div
         style={{
           fontSize: 128,
-          background: "black",
+          background: "linear-gradient(135deg, #c71585, #800080, #000000)",
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          textAlign: "center",
+          color: "white",
+          padding: "20px",
+          boxShadow: "0 4px 20px rgba(0, 0, 0, 0.5)",
+          borderRadius: "10px",
         }}
       >
         <span
           style={{
-            color: "white",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            letterSpacing: "5px",
           }}
         >
           A S H G W
