3ac98ff0d16288b49c97463c0df3525a03ca2d06
ashgw <ashrefgw@proton.me>
2025-03-21 22:54:14 +0100

    fix: nav shenanigans

--- apps/www/src/app/components/nav/utils/navLinks.ts
+++ apps/www/src/app/components/nav/utils/navLinks.ts
@@ -1,14 +1,12 @@
-type Name = "Home" | "Blog" | "Services" | "Contact";
+type Name = "Home" | "Blog" | "About";
 
 interface NavLink {
   name: Name;
   href: string;
-  hidden: boolean;
 }
 
 export const navLinks: readonly NavLink[] = [
-  { name: "Home", href: "/", hidden: false },
-  { name: "Blog", href: "/blog", hidden: false },
-  { name: "Services", href: "/services", hidden: true },
-  { name: "Contact", href: "/contact", hidden: false },
+  { name: "Home", href: "/" },
+  { name: "Blog", href: "/blog" },
+  { name: "About", href: "/about" },
 ];
