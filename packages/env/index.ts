acee2df9a9ca0dea4a79ec65541346a0f08525f5
ashgw <ashrefgw@proton.me>
2025-03-23 15:58:07 +0100

    fix: the env issue

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,15 +1,12 @@
 import path from "path";
-import type { UnionToTuple } from "ts-roids";
 import { config } from "dotenv";
 import { z } from "zod";
 
-import { createEnv, preset } from "@ashgw/ts-env";
+import { createEnv } from "@ashgw/ts-env";
 
 config({ path: path.resolve(__dirname, "../../.env") });
 
 const isBrowser = typeof window !== "undefined";
-
-const vercelPreset = preset("vercel");
 
 export const env = createEnv({
   vars: {
@@ -18,12 +15,17 @@ export const env = createEnv({
     BLOG_URL: z.string().url(),
     WWW_GOOGLE_ANALYTICS_ID: z.string().min(7).startsWith("G-"),
     BLOG_GOOGLE_ANALYTICS_ID: z.string().min(7).startsWith("G-"),
-    ...vercelPreset,
   },
-  disablePrefix: [
-    "NODE_ENV",
-    ...(Object.keys(vercelPreset) as UnionToTuple<keyof typeof vercelPreset>),
-  ],
+  disablePrefix: ["NODE_ENV"],
   prefix: "NEXT_PUBLIC",
+  runtimeEnv: {
+    NEXT_PUBLIC_WWW_GOOGLE_ANALYTICS_ID:
+      process.env.NEXT_PUBLIC_WWW_GOOGLE_ANALYTICS_ID,
+    NEXT_PUBLIC_BLOG_GOOGLE_ANALYTICS_ID:
+      process.env.NEXT_PUBLIC_BLOG_GOOGLE_ANALYTICS_ID,
+    NEXT_PUBLIC_WWW_URL: process.env.NEXT_PUBLIC_WWW_URL,
+    NEXT_PUBLIC_BLOG_URL: process.env.NEXT_PUBLIC_BLOG_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  },
   skipValidation: isBrowser, // Since env vars are already injected at build time, we don't need to validate them at runtime.
 });
