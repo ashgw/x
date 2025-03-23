2792d2fb8dfd2b240e288a24cb09bd5ee2082997
ashgw <ashrefgw@proton.me>
2025-03-23 01:22:51 +0100

    chore: add icons

--- packages/components/src/footer/components/CopyRight.tsx
+++ packages/components/src/footer/components/CopyRight.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { motion } from "framer-motion";
 
 import { CREATOR } from "@ashgw/constants";
@@ -8,16 +6,9 @@ export function CopyRight() {
   return (
     <motion.div
       viewport={{ once: true }} // only run once per load
-      whileInView={{
-        opacity: 1,
-      }}
-      initial={{
-        opacity: 0,
-      }}
-      transition={{
-        duration: 1,
-        ease: "easeInOut",
-      }}
+      whileInView={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
     >
       <p className="text-secondary-center dimmed-3 flex items-center justify-center text-sm">
         &copy; {new Date().getFullYear()} {CREATOR}. All rights reserved
