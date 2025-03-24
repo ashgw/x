9319cdf5f5c485bddad1f879a32b61c4e4c69914
ashgw <ashrefgw@proton.me>
2025-03-24 13:18:27 +0100

    feat: add global error catcher

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -4,8 +4,6 @@ import { createMetadata } from "@ashgw/seo";
 
 import { HomePage } from "~/app/components/pages/home";
 
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 export const metadata: Metadata = createMetadata({
   title: "Home",
   description: "Welcome home.",
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,3 +2,4 @@ export { Footer } from "./footer";
 export { NotFound } from "./not-found";
 export { Loading } from "./loading";
 export * from "./reusables";
+export { Error, type GlobalErrorProperties } from "./error";
