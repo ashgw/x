db6650c192de70e8c3bb85307f8bf71628e7b154
Ashref Gwader <ashref@ashgw.me>
2025-03-25 00:50:44 +0100

    chore: add new basic copy (#494)
    # Rewrite About page content with a more casual, personal tone
    
    ### TL;DR
    
    Completely rewrote the About page content to be more conversational, authentic, and less formal.
    
    The previous About page read like a formal resume with technical jargon and professional accomplishments. This update creates a more authentic, conversational tone that better reflects personality and creates a more engaging user experience. The new content reveals more about personal interests beyond coding while maintaining professionalism.

--- apps/www/src/app/components/pages/about/index.tsx
+++ apps/www/src/app/components/pages/about/index.tsx
@@ -3,28 +3,12 @@
 import React from "react";
 
 import { Footer } from "@ashgw/components";
-import { LINKS, REPO_SOURCE, SITE_NAME } from "@ashgw/constants";
+import { LINKS, REPO_SOURCE } from "@ashgw/constants";
 
 import AboutCard from "./components/AboutCard";
 import Link from "./components/Link";
 import MajorHeading from "./components/MajorHeading";
 import MinorHeading from "./components/MinorHeading";
-
-const ThisSiteTools = [
-  "TypeScript",
-  "ReactJS",
-  "Figma",
-  "NextJS",
-  "Framer-motion",
-  "TailwindCSS",
-  "MDX",
-  "GitHub Actions",
-  "Terraform",
-  "AWS",
-  "Playwright",
-  "Jest",
-  "Docker",
-];
 
 export function AboutPage() {
   return (
@@ -36,79 +20,92 @@ export function AboutPage() {
         </code>
         <AboutCard title="">
           <p>
-            I&apos;m a poker player, and a retired chess player, anthropology
-            aficionado, cinema enthusiast, and software engineer.
-          </p>
-        </AboutCard>
-        <AboutCard title="">
-          <p>
-            I&apos;ve been programming since, well as far as I can remember,
-            getting professionally paid for it for the last 5 years now, where
-            I&apos;ve had the privilege to collaborate with numerous
-            individuals, teams and startups worldwide, to build, scale, and lead
-            software projects.
-          </p>
-        </AboutCard>
-        <div className="hidden">
-          <MajorHeading id="stack" title="Stack" />
-        </div>
-        <AboutCard title="">
-          <p>
-            I&apos;ve done it all, been there, done that, all the way from bare
-            metal to frontend
-            <br />
-            (this site is open
-            <Link href={REPO_SOURCE} name="source" /> btw). <br />
-            As of now, I mostly enjoy and work with TypeScript, Docker and AWS.
-          </p>
-        </AboutCard>
-        <AboutCard title="">
-          <p>
-            I also do some open source from time to time, where you can find me
-            publishing libraries on <br />
-            <Link href={LINKS.npm} name="npm" />,
-            <Link href={LINKS.crates} name="crates" /> and
-            <Link href={LINKS.pypi} name="pypi" />
-          </p>
-        </AboutCard>
-        <AboutCard title="">
-          <p>
-            I am currently <Link href={"/services"} name="open" />
-            to engaging in selective consulting contracts and project
-            partnerships. If you&apos;re looking to scale SaaS products, or
-            enhance your team capabilities,
-            <br />
-            <Link href={"/contact"} name="let's talk." />
+            I&apos;d be glazing myself pretty hard if I wrote a whole section
+            here explaining who I am. I&apos;d rather you figure that out by
+            exploring my <Link href={"/blog"} name="blog" />, open source repos,
+            or clicking around at random. But since you&apos;re here, I&apos;m
+            guessing you saw me through something software-related.
           </p>
         </AboutCard>
 
-        <div id="about-this-site" className="hidden">
-          <MajorHeading title={SITE_NAME} />
-          <AboutCard title="">
-            <p>
-              This site is open
-              <Link href={REPO_SOURCE} name="source," /> and made using the
-              following technologies for prototyping, provisioning, development,
-              testing, and deployment.
-            </p>
-          </AboutCard>
-          <AboutCard title="">
-            <div className="dimmed-4 flex flex-wrap items-center justify-center gap-[0.625rem] text-sm">
-              {ThisSiteTools.map((tech) => (
-                <span
-                  key={tech}
-                  className="relative rounded-full border border-white/10 px-2 py-1"
-                >
-                  {tech}
-                </span>
-              ))}
-            </div>
-          </AboutCard>
-        </div>
+        <AboutCard title="">
+          <p>
+            I do some open source from time to time, so feel free to check out
+            my stuff on <Link inNewTab href={LINKS.npm} name="npm" />,{" "}
+            <Link inNewTab href={LINKS.crates} name="crates" />, and{" "}
+            <Link inNewTab href={LINKS.pypi} name="pypi" />. I also share a tiny
+            bit of code publicly on <Link href={LINKS.gitHub} name="GitHub" />,
+            mostly dev tools and niche libraries.
+          </p>
+        </AboutCard>
+        <AboutCard title="">
+          <p>
+            Software-wise, I&apos;m not a “backend developer” or “full-stack
+            engineer” or “iOS” engineer, and I don&apos;t like to box myself
+            into labels. If I don&apos;t know something, I&apos;ll learn it,
+            easy as.
+          </p>
+        </AboutCard>
+        <AboutCard title="">
+          <p>
+            I&apos;ve been on Linux since I was 12. Take a peek at my{" "}
+            <Link href={LINKS.gitHub + "/dotfiles"} name="dotfiles" /> if you
+            want to see how I run my setup. I don&apos;t stress about what some
+            random library calls its methods, frameworks come and go. It&apos;s
+            all the same to me, just pick the tool and keep it moving.
+          </p>
+        </AboutCard>
+        <AboutCard title="">
+          <p>
+            I&apos;ve built all kinds of stuff: web apps, mobile apps, heck even
+            GitHub Apps, no-code and code automations, firmwares (yes
+            firmwares), infra pipelines, LLM and agent integrations workflows,
+            low-level RFCs and protocols …whatever. If it compiles or even if it
+            doesn&apos;t, I&apos;ll figure it out. The only thing I recall now
+            working on is blockchain, and I probably won&apos;t pursue{" "}
+            <Link
+              inNewTab
+              href={
+                "https://www.reddit.com/r/memecoins/comments/1heta78/yeah_im_finna_bridge/"
+              }
+              name="that"
+            />{" "}
+            any time soon
+          </p>
+        </AboutCard>
+        <AboutCard title="">
+          <p>
+            That&apos;s about it on software, but there&apos;s more to me than
+            code. I&apos;m into psychology, philosophy, archaeology,
+            anthropology, and history, probably 5 other obsessions I&apos;m
+            forgetting right now. Maybe I&apos;ll spin up dedicated blog
+            sections for each topic like I&apos;m doing here. Who knows.
+          </p>
+        </AboutCard>
+        <AboutCard title="">
+          <p>
+            College was too slow for me, so I started skipping. Then they said,
+            “You can&apos;t pass if you don&apos;t show up,” so I dropped out.
+            ROI wasn&apos;t there anyway. I learn 10x faster on my own. So no,
+            this isn&apos;t a fancy CV or &quot;portfolio&quot; showcasing what
+            a great &quot;employee&quot; I am. I&apos;m not that type, and I
+            don&apos;t use CVs.
+          </p>
+        </AboutCard>
+        <AboutCard title="">
+          <p>
+            Oh, and there&apos;s a good chance I&apos;d beat you at poker,
+            chess, or pool (especially poker).{" "}
+            <Link href={LINKS.twitter.link} name="hmu rn" />. But hey,
+            that&apos;s enough about me for now. The entire site is{" "}
+            <Link href={REPO_SOURCE} name="open source" /> by the way, so feel
+            free to rummage around. Peace out.
+          </p>
+        </AboutCard>
       </div>
       <div className="mt-10"></div>
       <MinorHeading title="" />
-      <Footer></Footer>
+      <Footer />
       <div className="h-10"></div>
     </>
   );
