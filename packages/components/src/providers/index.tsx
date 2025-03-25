b3491e9eddf52b1e955393e7cb8dfb49f60f0957
ashgw <ashrefgw@proton.me>
2025-03-25 03:22:05 +0100

    feat: add global providers

---
+++ packages/components/src/providers/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import type { PropsWithChildren } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+import { AnalyticsProvider } from "@ashgw/analytics";
+
+interface ProvidersProps extends PropsWithChildren {
+  site: "blog" | "www";
+}
+
+export function Providers({ children, site }: ProvidersProps) {
+  return (
+    <AnalyticsProvider site={site}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <NextUIProvider>{children}</NextUIProvider>
+      </NextThemesProvider>
+    </AnalyticsProvider>
+  );
+}
