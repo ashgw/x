a9ca315db9a576d41cf40c28d40177ee403e575f
ashgw <ashrefgw@proton.me>
2025-03-26 23:20:13 +0100

    fix: the static site params build

--- apps/blog/src/app/(pages)/[post]/page.tsx
+++ apps/blog/src/app/(pages)/[post]/page.tsx
@@ -1,17 +1,15 @@
 import { BlogPost } from "~/app/components/pages/[post]";
 import { MdxService } from "~/lib";
 
-// export const generateStaticParams = async () => {
-//   const posts = await new MdxService("public/blogs").getPosts();
-//   return posts.map((post) => ({ post: post.filename }));
-// };
-
-interface PageProps {
-  params: Promise<{ postName: string }>;
+export const generateStaticParams = async () => {
+  const posts = await new MdxService("public/blogs").getPosts();
+  return posts.map((post) => ({ post: post.filename }));
+};
+interface DynamicRouteParams {
+  params: { post: string };
 }
 
-export default async function Page({ params }: PageProps) {
-  const { postName } = await params;
-  const postData = await new MdxService("public/blogs").getPost(postName);
+export default async function Page({ params }: DynamicRouteParams) {
+  const postData = await new MdxService("public/blogs").getPost(params.post);
   return <BlogPost postData={postData} />;
 }
