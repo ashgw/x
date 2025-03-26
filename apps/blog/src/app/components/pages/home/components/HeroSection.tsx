05cc54471cc4f1880c39eb86c977843f146c3858
ashgw <ashrefgw@proton.me>
2025-03-26 13:48:11 +0100

    chore: add fine shyt

--- apps/blog/src/app/components/pages/home/components/HeroSection.tsx
+++ apps/blog/src/app/components/pages/home/components/HeroSection.tsx
@@ -1,31 +1,20 @@
-import type { Metadata } from "next";
-import { Suspense } from "react";
-import { notFound } from "next/navigation";
-import Posts from "@/app/components/post/posts";
-import { CREATOR } from "@/lib/constants";
-import { pub } from "@/lib/env";
-import { getSiteName } from "@/lib/funcs/site-name";
-import { getBlogPosts } from "@/lib/mdx/content";
-
-import { LoadingScreen } from "@ashgw/components";
+import { MdxService } from "~/lib/index";
 
 export default async function BlogPage() {
-  const posts = await getBlogPosts();
+  const posts = await new MdxService(
+    "./../../../../../../public/blogs",
+  ).getBlogPosts();
 
-  if (posts) {
-    return (
-      <Suspense fallback={<LoadingScreen />}>
-        <section className="container mx-auto sm:max-w-xl md:max-w-3xl lg:max-w-3xl xl:max-w-3xl">
-          <h1 className="mb-8 hidden text-2xl font-medium tracking-tighter">
-            Unclassified, raw
-          </h1>
-          <Posts posts={posts} />
-          <div className="h-full w-auto"></div>
-        </section>
-        <div className="py-6"></div>
-      </Suspense>
-    );
-  } else {
-    return notFound();
-  }
+  return (
+    <>
+      <section className="container mx-auto sm:max-w-xl md:max-w-3xl lg:max-w-3xl xl:max-w-3xl">
+        <h1 className="mb-8 hidden text-2xl font-medium tracking-tighter">
+          Unclassified, raw
+        </h1>
+        <Posts posts={posts} />
+        <div className="h-full w-auto"></div>
+      </section>
+      <div className="py-6"></div>
+    </>
+  );
 }
