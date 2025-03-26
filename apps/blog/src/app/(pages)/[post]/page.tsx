aa23039cd8ec7e94e68f9d2a0b5bcc874ded7bb6
ashgw <ashrefgw@proton.me>
2025-03-26 23:25:36 +0100

    feat: add generate metadat and make it work

--- apps/blog/src/app/(pages)/[post]/page.tsx
+++ apps/blog/src/app/(pages)/[post]/page.tsx
@@ -1,3 +1,5 @@
+import { createMetadata } from "@ashgw/seo";
+
 import { BlogPost } from "~/app/components/pages/[post]";
 import { MdxService } from "~/lib";
 
@@ -5,8 +7,19 @@ export const generateStaticParams = async () => {
   const posts = await new MdxService("public/blogs").getPosts();
   return posts.map((post) => ({ post: post.filename }));
 };
+
 interface DynamicRouteParams {
   params: { post: string };
+}
+
+export async function generateMetadata({ params }: DynamicRouteParams) {
+  const postData = await new MdxService("public/blogs").getPost(params.post);
+
+  return createMetadata({
+    title: postData.filename,
+    description: postData.parsedContent.attributes.seoTitle,
+    keywords: postData.parsedContent.attributes.tags,
+  });
 }
 
 export default async function Page({ params }: DynamicRouteParams) {
