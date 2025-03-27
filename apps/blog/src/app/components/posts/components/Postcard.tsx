1841b337463fa02f66c6cdd59c49e1283c154964
ashgw <ashrefgw@proton.me>
2025-03-27 17:21:47 +0100

    feat: make blog tags rounded too

--- apps/blog/src/app/components/posts/components/Postcard.tsx
+++ apps/blog/src/app/components/posts/components/Postcard.tsx
@@ -30,7 +30,7 @@ export function PostCard({ postData }: { postData: PostData }) {
                   <Link
                     href={`/tag/${tag}`}
                     key={tag}
-                    className="relative rounded-full px-2 py-1"
+                    className="relative rounded-full border border-white/10 px-4 py-2 text-sm transition-all duration-200 ease-in-out hover:border-white/20"
                   >
                     {tag}
                   </Link>
