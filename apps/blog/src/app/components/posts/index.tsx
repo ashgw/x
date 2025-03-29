cecf1298b87f83d0a1dae35a0af1d6f54948c570
ashgw <ashrefgw@proton.me>
2025-03-29 03:44:32 +0100

    feat: enhance the back button

--- apps/blog/src/app/components/posts/index.tsx
+++ apps/blog/src/app/components/posts/index.tsx
@@ -73,7 +73,7 @@ export function Posts({ posts }: PostsProps) {
       onClick={() => setSelectedCategory(category)}
       className={`rounded-xl px-4 py-2 transition-all duration-200 ${
         selectedCategory === category
-          ? "border border-white/30 bg-white/10 text-white"
+          ? "border border-white/30 bg-white/5 text-white"
           : "dimmed-3 hover:dimmed-4 border border-white/10 hover:border-white/40"
       }`}
     >
