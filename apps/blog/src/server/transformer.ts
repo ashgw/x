5665bb11251aef5246ee1bdfccde52c3fb6593f0
ashgw <ashrefgw@proton.me>
2025-04-14 18:49:28 +0100

    chore: add basic templates

--- apps/blog/src/server/transformer.ts
+++ apps/blog/src/server/transformer.ts
@@ -1,3 +1,7 @@
 import superjson from "superjson";
 
-export const transformer = superjson;
+// this can be transfered with the tRPC package tho
+export const transformer = superjson as {
+  serialize: typeof superjson.serialize;
+  deserialize: typeof superjson.deserialize;
+};
