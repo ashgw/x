78a71753176554f2fbf8040d761fbc6921372401
ashgw <ashrefgw@proton.me>
2025-04-14 18:35:17 +0100

    fix: even more fixes

--- apps/blog/src/server/trpc.ts
+++ apps/blog/src/server/trpc.ts
@@ -1,8 +1,22 @@
 import { initTRPC } from "@trpc/server";
+import { ZodError } from "zod";
 
 import type { Context } from "./context";
+import { transformer } from "./transformer";
 
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  transformer,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
