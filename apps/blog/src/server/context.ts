fc1d09302734636b1c701f5351bbabcdc9de5d56
ashgw <ashrefgw@proton.me>
2025-04-14 15:55:27 +0100

    fix: @deprecation warning

--- apps/blog/src/server/context.ts
+++ apps/blog/src/server/context.ts
@@ -1,14 +1,10 @@
-import type { inferAsyncReturnType } from "@trpc/server";
 import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 
 export function createContext(_opts: FetchCreateContextFnOptions) {
-  const user = _opts.req.headers.get("username") ?? "anonymous";
-
   return {
-    user,
     // Add other blog-specific context here
     // Example: prisma: new PrismaClient()
   };
 }
 
-export type Context = inferAsyncReturnType<typeof createContext>;
+export type Context = Awaited<ReturnType<typeof createContext>>;
