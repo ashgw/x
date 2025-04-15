6c3ea6e6bd5ca5ea0026ebb06cd81a2fd947de83
ashgw <ashrefgw@proton.me>
2025-04-15 01:13:44 +0100

    fix: tags

--- apps/blog/src/server/models/blog/dtos.ts
+++ apps/blog/src/server/models/blog/dtos.ts
@@ -1,14 +1,13 @@
 import { z } from "zod";
 
-export const getBlogsDtoSchema = z.object({
-  blogPath: z.string().min(1),
-});
-
-export type GetBlogsDto = z.infer<typeof getBlogsDtoSchema>;
-
 export const getBlogDtoSchema = z.object({
   filename: z.string().min(1),
   blogPath: z.string().min(1),
 });
 
+export const getBlogsDtoSchema = z.object({
+  blogPath: z.string().min(1),
+});
+
+export type GetBlogsDto = z.infer<typeof getBlogsDtoSchema>;
 export type GetBlogDto = z.infer<typeof getBlogDtoSchema>;
