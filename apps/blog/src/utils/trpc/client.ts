9cae8c26c96763bb5f5b9a278746c82078b9fc89
ashgw <ashrefgw@proton.me>
2025-04-16 16:23:22 +0100

    chore: add query client

--- apps/blog/src/utils/trpc/client.ts
+++ apps/blog/src/utils/trpc/client.ts
@@ -1,5 +1,22 @@
+import type { QueryClient } from "@tanstack/react-query";
 import { createTRPCReact } from "@trpc/react-query";
 
 import type { AppRouter } from "~/server/root";
+import { makeQueryClient } from "./query-client";
 
 export const client = createTRPCReact<AppRouter>();
+
+let clientQueryClientSingleton: QueryClient | null = null;
+
+export function getQueryClient() {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  }
+  // Browser: use singleton pattern to keep the same query client
+  return (clientQueryClientSingleton ??= makeQueryClient());
+}
+
+export function getTrpcUrl(input: { siteBaseUrl: string }) {
+  return typeof window !== "undefined" ? "" : `${input.siteBaseUrl}/api/trpc`;
+}
