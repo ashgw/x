14b2429ae1293e6bea0f98e953e96270db220fc4
ashgw <ashrefgw@proton.me>
2025-04-16 18:32:40 +0100

    fix: make caller work

--- apps/blog/src/trpc/context.ts
+++ apps/blog/src/trpc/context.ts
@@ -1,16 +1,15 @@
 import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import type { NextRequest, NextResponse } from "next/server";
 
-export function createTRPCContext(
-  opts: FetchCreateContextFnOptions & {
-    req: NextRequest;
-    res: NextResponse;
-  },
-) {
+export function createTRPCContext(opts: {
+  req: NextRequest;
+  res: NextResponse;
+  trpcInfo: FetchCreateContextFnOptions["info"];
+}) {
   return {
     req: opts.req,
     res: opts.res,
-    trpcInfo: opts.info,
+    trpcInfo: opts.trpcInfo,
   };
 }
 
--- apps/blog/src/trpc/server.ts
+++ apps/blog/src/trpc/server.ts
@@ -1,5 +1,7 @@
-import "server-only"; // DO NOT DELET THIS DAWG
+import "server-only"; // DO NOT DELETE THIS DAWG
 
+import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+import type { NextRequest, NextResponse } from "next/server";
 import { cache } from "react";
 import { createHydrationHelpers } from "@trpc/react-query/rsc";
 
@@ -12,9 +14,18 @@ import { makeQueryClient } from "./query-client";
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 
-export const getQueryClient = cache(makeQueryClient);
+const getQueryClient = cache(makeQueryClient);
 
-const caller = createCallerFactory(appRouter)(createTRPCContext);
+const createCaller = createCallerFactory(appRouter);
+
+const caller = createCaller(
+  createTRPCContext({
+    // since these are empty do not use them for anything other than server side simple fetching, don't check for them
+    req: {} as NextRequest,
+    res: {} as NextResponse,
+    trpcInfo: {} as FetchCreateContextFnOptions["info"],
+  }),
+);
 
 export const { trpc: trpcServer, HydrateClient } =
   createHydrationHelpers<AppRouter>(caller, getQueryClient);
