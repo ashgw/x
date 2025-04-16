4a082a7a0bac456a8b2e22a9a5c09f798eb7da79
ashgw <ashrefgw@proton.me>
2025-04-16 18:06:55 +0100

    chore: batch

--- packages/observabiliy/src/sentry/nextJs/captureException.ts
+++ packages/observabiliy/src/sentry/nextJs/captureException.ts
@@ -5,6 +5,16 @@ import { logger } from "../../log";
 type Exception = Parameters<typeof sentryCaptureException>[0];
 type Hint = Parameters<typeof sentryCaptureException>[1];
 
+/**
+ * Captures an exception and logs it using Sentry and a centralized logger.
+ *
+ * @param {Object} params - The parameters for capturing the exception.
+ * @param {Exception} params.error - The error to be captured.
+ * @param {Hint} [params.hint] - Optional hint for additional context.
+ * @param {Object} [params.withErrorLogging] - Optional object for logging additional error messages.
+ * @param {string} params.withErrorLogging.message - The message to log if provided.
+ * @returns {string} The extracted error message.
+ */
 export const captureException = ({
   error,
   hint,
