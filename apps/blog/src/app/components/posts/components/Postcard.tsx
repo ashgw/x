5136f82433e85679d5fd92d801e7650a711b532e
ashgw <ashrefgw@proton.me>
2025-04-16 18:56:13 +0100

    fix: lint issue

--- apps/blog/src/app/components/posts/components/Postcard.tsx
+++ apps/blog/src/app/components/posts/components/Postcard.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 import { DateService } from "@ashgw/cross-runtime";
 
-import type { PostData } from "~/lib/mdx";
+import type { PostDataRo } from "~/server/models";
 
-export function PostCard({ postData }: { postData: PostData }) {
+export function PostCard({ postData }: { postData: PostDataRo }) {
   return (
     <>
       {postData.parsedContent.attributes.isReleased ||
@@ -14,7 +14,8 @@ export function PostCard({ postData }: { postData: PostData }) {
             <div className="flex flex-col gap-4 lg:gap-6">
               <li className="dimmed-0 ml-5 list-disc">
                 {DateService.formatDate({
-                  stringDate: postData.parsedContent.attributes.firstModDate,
+                  stringDate:
+                    postData.parsedContent.attributes.firstModDate.toISOString(),
                 })}
               </li>
               <Link href={`/${postData.filename}`}>
