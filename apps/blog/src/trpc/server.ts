8ad2fdab00021ffd2079cfb8143e22fd1d5edafd
ashgw <ashrefgw@proton.me>
2025-04-16 16:51:46 +0100

    chore: cleanup

--- apps/blog/src/trpc/server.ts
+++ apps/blog/src/trpc/server.ts
@@ -1,8 +1,9 @@
-import "server-only"; // <-- ensure this file cannot be imported from the client
+import "server-only"; // DO NOT DELET THIS DAWG
 
 import { cache } from "react";
 import { createHydrationHelpers } from "@trpc/react-query/rsc";
 
+import type { AppRouter } from "~/server/router";
 import { appRouter } from "~/server/router";
 import { createCallerFactory } from "~/trpc/trpc";
 import { createTRPCContext } from "./contex";
@@ -14,7 +15,6 @@ import { makeQueryClient } from "./query-client";
 export const getQueryClient = cache(makeQueryClient);
 
 const caller = createCallerFactory(appRouter)(createTRPCContext);
-export const { trpc, HydrateClient } = createHydrationHelpers<typeof appRouter>(
-  caller,
-  getQueryClient,
-);
+
+export const { trpc: trpcServer, HydrateClient } =
+  createHydrationHelpers<AppRouter>(caller, getQueryClient);
