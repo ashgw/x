e83d8c51858541cd83b14f06200260251996632e
ashgw <ashrefgw@proton.me>
2025-04-16 16:56:59 +0100

    ci: add naming convention

--- apps/blog/src/server/models/blog/dtos.ts
+++ apps/blog/src/server/models/blog/dtos.ts
@@ -1,3 +1,7 @@
+// Follow this naming convention for zod schemas and types
+// <action>-<Entity(s)>-dtoSchema for zod schemas
+// <Action>-<Entity(s)>-dto for the types of the dtos
+
 import { z } from "zod";
 
 export const getBlogDtoSchema = z.object({
