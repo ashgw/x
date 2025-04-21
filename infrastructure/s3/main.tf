dfc8513fe42b97cedce5293db001f5f3d6355a0d
ashgw <ashrefgw@proton.me>
2025-04-21 20:01:31 +0100

    feat(infra): provision with folders

--- infrastructure/s3/main.tf
+++ infrastructure/s3/main.tf
@@ -128,3 +128,44 @@ resource "aws_s3_bucket_public_access_block" "prod_bucket_public_access_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# Create default folders in each bucket
+locals {
+  buckets = {
+    dev     = aws_s3_bucket.dev_bucket
+    preview = aws_s3_bucket.preview_bucket
+    prod    = aws_s3_bucket.prod_bucket
+  }
+}
+
+resource "aws_s3_object" "audio_folder" {
+  for_each = local.buckets
+
+  bucket  = each.value.id
+  key     = "audio/"
+  content = "" # Empty content as this is just a prefix/folder
+}
+
+resource "aws_s3_object" "mdx_folder" {
+  for_each = local.buckets
+
+  bucket  = each.value.id
+  key     = "mdx/"
+  content = ""
+}
+
+resource "aws_s3_object" "image_folder" {
+  for_each = local.buckets
+
+  bucket  = each.value.id
+  key     = "image/"
+  content = "" 
+}
+
+resource "aws_s3_object" "other_folder" {
+  for_each = local.buckets
+
+  bucket  = each.value.id
+  key     = "other/"
+  content = ""  # things that aren't for my blog, can be any other blob
+}
