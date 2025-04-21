818d13309dbd830a73245b56dbfaf265ac76eb9c
ashgw <ashrefgw@proton.me>
2025-04-21 01:51:36 +0100

    fix: types mismatch

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,17 +4,20 @@ import { neonConfig, Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import ws from "ws";
 
-import { PrismaClient } from "../generated/client";
-import { keys } from "../keys";
+import { env } from "@ashgw/env";
+
+import { PrismaClient } from "./generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 neonConfig.webSocketConstructor = ws;
 
-const pool = new Pool({ connectionString: keys().DATABASE_URL });
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
 const adapter = new PrismaNeon(pool);
 
-export const database = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const database = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = database;
