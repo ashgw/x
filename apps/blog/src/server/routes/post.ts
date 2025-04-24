e7833b6a6d4d18f6c2ccb0b2580ba18e5a557934
ashgw <ashrefgw@proton.me>
2025-04-24 16:58:36 +0100

    fix: pools

--- apps/blog/src/server/routes/post.ts
+++ apps/blog/src/server/routes/post.ts
@@ -4,12 +4,14 @@ import { publicProcedure, router } from "../../trpc/trpc";
 import { getPostDtoSchema, postDataSchemaRo } from "../models";
 import { BlogService } from "../services/blog";
 
+const blogService = new BlogService(); // for pools
+
 export const postRouter = router({
   getPost: publicProcedure
     .input(getPostDtoSchema)
     .output(postDataSchemaRo)
     .query(async ({ input }) => {
-      const post = await new BlogService().getPost({
+      const post = await blogService.getPost({
         filename: input.filename,
       });
       return post;
@@ -18,7 +20,7 @@ export const postRouter = router({
   getPosts: publicProcedure
     .output(z.array(postDataSchemaRo))
     .query(async () => {
-      const posts = await new BlogService().getPosts();
+      const posts = await blogService.getPosts();
       return posts;
     }),
 });
