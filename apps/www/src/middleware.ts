07822c081fbdff2513b77d8a0c9649b4dce5b837
ashgw <ashrefgw@proton.me>
2025-04-25 10:43:18 +0100

    feat: add redirect booking

--- apps/www/src/middleware.ts
+++ apps/www/src/middleware.ts
@@ -1,14 +1,23 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+import { BOOKING_LINK } from "@ashgw/constants";
+
 export function middleware(request: NextRequest) {
   const { pathname, search } = request.nextUrl;
-  // eslint-disable-next-line no-restricted-properties
-  const targetBaseUrl = process.env.NEXT_PUBLIC_BLOG_URL;
 
+  // Blog redirect
   if (pathname.startsWith("/blog")) {
-    // we needa remove the /blog prefix from the pathname ofc
+    // eslint-disable-next-line no-restricted-properties
+    const targetBaseUrl = process.env.NEXT_PUBLIC_BLOG_URL;
     const cleanPath = pathname.replace(/^\/blog/, "");
+    const targetUrl = `${targetBaseUrl}${cleanPath}${search}`;
+    return NextResponse.redirect(targetUrl);
+  }
+
+  if (pathname.startsWith("/booking")) {
+    const targetBaseUrl = BOOKING_LINK;
+    const cleanPath = pathname.replace(/^\/booking/, "");
     const targetUrl = `${targetBaseUrl}${cleanPath}${search}`;
     return NextResponse.redirect(targetUrl);
   }
@@ -17,5 +26,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/blog/:path*"],
+  matcher: ["/blog/:path*", "/booking/:path*"],
 };
