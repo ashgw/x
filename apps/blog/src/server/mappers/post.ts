aa02975aacdb9b74e163b47361894849416942e4
ashgw <ashrefgw@proton.me>
2025-05-04 19:52:32 +0100

    chore: use mappers

--- apps/blog/src/server/mappers/post.ts
+++ apps/blog/src/server/mappers/post.ts
@@ -1,9 +1,37 @@
-import type { Post } from "@ashgw/db/raw";
+import type { Post, PostCategory } from "@ashgw/db/raw";
 
 import type { PostDataRo } from "../models";
 
 export class PostMapper {
-  public static ro({ _post }: { post: Post }): PostDataRo {
-    return {};
+  public static ro({ post }: { post: Post }): PostDataRo {
+    return {
+      filename: post.slug,
+      parsedContent: {
+        attributes: {
+          firstModDate: post.firstModDate,
+          isReleased: post.isReleased,
+          isSequel: post.isSequel,
+          lastModDate: post.lastModDate,
+          minutesToRead: post.minutesToRead,
+          seoTitle: post.seoTitle,
+          summary: post.summary,
+          tags: post.tags,
+          title: post.title,
+          category: this.mapCategory(post.category),
+        },
+      },
+    };
+  }
+  private static mapCategory(
+    category: PostCategory,
+  ): PostDataRo["parsedContent"]["attributes"]["category"] {
+    switch (category) {
+      case "HEALTH":
+        return "health";
+      case "PHILOSOPHY":
+        return "philosophy";
+      case "SOFTWARE":
+        return "software";
+    }
   }
 }
--- packages/db/src/raw.ts
+++ packages/db/src/raw.ts
@@ -1,1 +1,25 @@
+import type {
+  ActionType,
+  Post,
+  PostCategory,
+  ResourceType,
+  Upload,
+  UploadType,
+  User,
+  UserPermission,
+} from "./generated/client";
+
+// Explicitly re-export the types
+export type {
+  Post,
+  PostCategory,
+  Upload,
+  ActionType,
+  ResourceType,
+  UploadType,
+  User,
+  UserPermission,
+};
+
+// Re-export everything else
 export * from "./generated/client";
