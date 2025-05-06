481caf6aea04a81a68d2a0ee1748914dd813d7c7
ashgw <ashrefgw@proton.me>
2025-05-06 19:27:36 +0100

    fix: frotnend shi

--- apps/blog/src/app/components/pages/home/index.tsx
+++ apps/blog/src/app/components/pages/home/index.tsx
@@ -3,6 +3,5 @@ import { PostCardsPage } from "./components/PostCardsPage";
 
 export async function HomePage() {
   const posts = await trpcServerSideClient.post.getPostCards();
-
   return <PostCardsPage posts={posts} />;
 }
