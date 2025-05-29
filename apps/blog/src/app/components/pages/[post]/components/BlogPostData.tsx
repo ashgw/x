4ba980dd4cd4739d797e971f61c8e48df6ad2488
ashgw <ashrefgw@proton.me>
2025-05-29 23:46:00 +0100

    feat: add the edit button

--- apps/blog/src/app/components/pages/[post]/components/BlogPostData.tsx
+++ apps/blog/src/app/components/pages/[post]/components/BlogPostData.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
+import { Edit } from "lucide-react";
+
 import { DateService } from "@ashgw/cross-runtime";
-import { Badge } from "@ashgw/ui";
+import { Badge, Button } from "@ashgw/ui";
 
 import type { PostDetailRo } from "~/api/models";
 import {
@@ -25,7 +28,18 @@ const featuredComponents = {
 export function BlogPostData({ postData }: BlogPostPorps) {
   return (
     <section className="container mx-auto sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-4xl">
-      <H1 id={postData.title}>{postData.title}</H1>
+      <div className="flex items-center justify-between">
+        <H1 id={postData.title}>{postData.title}</H1>
+        <Link href={`/editor?blog=${postData.slug}`} className="ml-4">
+          <Button
+            variant="outline"
+            size="icon"
+            className="h-9 w-9 rounded-full"
+          >
+            <Edit className="h-4 w-4" />
+          </Button>
+        </Link>
+      </div>
       <div className="mb-8 flex items-center justify-between text-sm sm:max-w-[450px] md:max-w-[550px] lg:max-w-[650px] xl:max-w-[750px]">
         <ReleaseDate date={postData.firstModDate.toISOString()} />
         <div>
