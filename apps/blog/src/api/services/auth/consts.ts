a0732a2baea9b6fa7798276da9bd451ad3d4d531
ashgw <ashrefgw@proton.me>
2025-05-30 21:48:53 +0100

    feat: add csrf token implementation for front

--- apps/blog/src/api/services/auth/consts.ts
+++ apps/blog/src/api/services/auth/consts.ts
@@ -1,7 +1,7 @@
 export const AUTH_COOKIES_MAX_AGE = 1000 * 60 * 60 * 24 * 7; // 7 days
 
 export const COOKIE_NAMES = {
-  CSRF_TOKEN: "_csrf_token",
+  CSRF_TOKEN: "csrf_token",
   SESSION_ID: "_lksx_ssid",
 } as const;
 
--- apps/blog/src/trpc/provider.tsx
+++ apps/blog/src/trpc/provider.tsx
@@ -4,17 +4,15 @@ import { useState } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 
-import { HEADER_NAMES } from "~/api/services/auth/consts";
+import { COOKIE_NAMES, HEADER_NAMES } from "~/api/services/auth/consts";
 import { getQueryClient, getTrpcUrl, trpcClientSide } from "./client";
 import { transformer } from "./transformer";
 
-function getCsrfTokenCookie(): string {
-  const csrfToken = document.cookie
+const getCsrfTokenCookie = (): string =>
+  document.cookie
     .split("; ")
-    .find((row) => row.startsWith("csrf_token="))
-    ?.split("=")[1];
-  return csrfToken ?? "";
-}
+    .find((row) => row.startsWith(COOKIE_NAMES.CSRF_TOKEN))
+    ?.split("=")[1] ?? "";
 
 export function TRPCProvider(
   props: Readonly<{
