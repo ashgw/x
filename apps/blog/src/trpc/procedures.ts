348112df61ecbc7b8d0d35cc3af74b6dc4574700
ashgw <ashrefgw@proton.me>
2025-06-24 15:55:00 +0100

    feat: add timing middleware

--- apps/blog/src/trpc/procedures.ts
+++ apps/blog/src/trpc/procedures.ts
@@ -1,8 +1,9 @@
 import { UserRoleEnum } from "~/api/models";
 import { authMiddleware } from "./middlewares/auth";
+import { timingMiddleware } from "./middlewares/dev-latency";
 import { procedure } from "./root";
 
-export const publicProcedure = procedure;
+export const publicProcedure = procedure.use(timingMiddleware);
 
 export const adminProcedure = publicProcedure.use(
   authMiddleware({
