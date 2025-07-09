91e1b54330b97df029a1e660d95dd88375693515
ashgw <ashrefgw@proton.me>
2025-07-09 12:57:17 +0100

    chore: use logtail on the low

--- packages/observabiliy/src/log.ts
+++ packages/observabiliy/src/log.ts
@@ -1,8 +1,17 @@
 /* eslint-disable */
+import { NextConfig } from "next";
+import { log, withLogtail } from "@logtail/next";
+
+// import { log } from "@logtail/next";
 import { env } from "@ashgw/env";
 
 // add OT if shit get serious
-const thirdParty = console; // here plug in any provider
+const thirdParty = console; // would use logtail, but no need for now
 const defaultLogger = console;
 export const logger =
   env.NODE_ENV === "production" ? thirdParty : defaultLogger;
+
+// not used rn
+export const withLogging = (config: NextConfig): NextConfig => {
+  return withLogtail(config);
+};
