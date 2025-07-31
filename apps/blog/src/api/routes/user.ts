bc245a55ef489f0319dbc1b39ce8c081161a4f3c
ashgw <ashrefgw@proton.me>
2025-08-01 00:59:33 +0100

    refactor: the auth funcs

--- apps/blog/src/api/routes/user.ts
+++ apps/blog/src/api/routes/user.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+import type { TrpcContext } from "~/trpc/context";
 import { publicProcedure } from "~/trpc/procedures";
 import { router } from "~/trpc/root";
 import {
@@ -9,48 +10,39 @@ import {
 } from "../models";
 import { AuthService } from "../services";
 
+const auth = (ctx: TrpcContext) =>
+  new AuthService({
+    db: ctx.db,
+    req: ctx.req,
+    res: ctx.res,
+  });
+
 export const userRouter = router({
   me: publicProcedure
     .input(z.void())
     .output(userSchemaRo.nullable()) // null if user not found
     .query(async ({ ctx }) => {
-      return await new AuthService({
-        db: ctx.db,
-        req: ctx.req,
-        res: ctx.res,
-      }).me();
+      return await auth(ctx).me();
     }),
 
   register: publicProcedure
     .input(userRegisterSchemaDto)
     .output(z.void())
     .mutation(async ({ input, ctx }) => {
-      return await new AuthService({
-        db: ctx.db,
-        req: ctx.req,
-        res: ctx.res,
-      }).register(input);
+      return await auth(ctx).register(input);
     }),
 
   login: publicProcedure
     .input(userLoginSchemaDto)
     .output(userSchemaRo)
     .mutation(async ({ input, ctx }) => {
-      return await new AuthService({
-        db: ctx.db,
-        req: ctx.req,
-        res: ctx.res,
-      }).login(input);
+      return await auth(ctx).login(input);
     }),
 
   logout: publicProcedure
     .input(z.void())
     .output(z.void())
     .mutation(async ({ ctx }) => {
-      return await new AuthService({
-        db: ctx.db,
-        req: ctx.req,
-        res: ctx.res,
-      }).logout();
+      return await auth(ctx).logout();
     }),
 });
