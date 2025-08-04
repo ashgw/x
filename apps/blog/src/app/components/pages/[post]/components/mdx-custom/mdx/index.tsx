dca3249aab7a9e2edbea398c2c4cb459a257d34d
ashgw <ashrefgw@proton.me>
2025-08-04 14:47:27 +0100

    refactor: mdx II

--- apps/blog/src/app/components/pages/[post]/components/mdx-custom/mdx/index.tsx
+++ apps/blog/src/app/components/pages/[post]/components/mdx-custom/mdx/index.tsx
@@ -6,7 +6,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import { TextContent } from "@ashgw/components";
 
 import { CodeBlock } from "../code";
-import { Divider } from "../Divider";
+import { Divider } from "../divider";
 import { H1, H2, H3 } from "../headers";
 import { BlogLink } from "../link/Link";
 import { Spacer1, Spacer2, Spacer3 } from "../spacers";
