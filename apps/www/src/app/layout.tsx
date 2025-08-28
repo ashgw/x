285ae462a8254b2a739bf7a4a0939692d7e19777
ashgw <ashrefgw@proton.me>
2025-08-28 01:12:46 +0100

    fix: copy

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -15,7 +15,7 @@ import { fonts } from "@ashgw/ui";
 import { NavBar } from "~/app/components/misc/nav";
 import { env } from "@ashgw/env";
 
-const description = "Building the future.";
+const description = "Building.";
 
 const siteUrl = env.NEXT_PUBLIC_WWW_URL;
 
