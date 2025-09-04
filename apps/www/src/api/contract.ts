f4e3335c8a580c98bd3b9fb5a994faabe4281627
ashgw <ashrefgw@proton.me>
2025-09-04 12:37:02 +0100

    chore: use recent api

--- apps/www/src/api/contract.ts
+++ apps/www/src/api/contract.ts
@@ -1,6 +1,6 @@
 import { initContract } from "@ts-rest/core";
 import { z } from "zod";
-import { cacheControlsQueryDtoSchema, errorSchemaRo } from "./rest/schemas";
+import { cacheControlsQueryDtoSchema, errorSchemaRo } from "./schemas";
 
 const c = initContract();
 
--- apps/www/src/app/api/v1/[...ts-rest]/route.ts
+++ apps/www/src/app/api/v1/[...ts-rest]/route.ts
@@ -1,34 +1,22 @@
-import { createNextHandler } from "@ts-rest/serverless/next";
+import { logger, monitor } from "@ashgw/observability";
+import { createNextHandler, tsr } from "@ts-rest/serverless/next";
 import { v1Contract } from "~/api/contract";
 import { Controllers } from "~/api/controllers";
-import { logger, monitor } from "@ashgw/observability";
 
-export const runtime = "nodejs";
+const router = tsr.router(v1Contract, {
+  bootstrap: async ({ query }) => Controllers.bootstrap({ q: query }),
+  gpg: async ({ query }) => Controllers.gpg({ q: query }),
+  debion: async ({ query }) => Controllers.debion({ q: query }),
+});
 
-const handler = createNextHandler(
-  v1Contract,
-  {
-    bootstrap: async ({ query }) => Controllers.bootstrap({ q: query }),
-    gpg: async ({ query }) => Controllers.gpg({ q: query }),
-    debion: async ({ query }) => Controllers.debion({ q: query }),
+const handler = createNextHandler(v1Contract, router, {
+  handlerType: "app-router",
+  basePath: "/api/v1",
+  responseValidation: true,
+  errorHandler(error, req) {
+    logger.error(`>>> ts-rest Error on '${req.url}'`, error);
+    monitor.next.captureException({ error });
   },
-  {
-    handlerType: "app-router",
-    basePath: "/api/v1",
-    responseValidation: true,
-    errorHandler(error, req) {
-      logger.error(`>>> ts-rest Error on '${req.url}'`, error);
-      monitor.next.captureException({ error });
-    },
-    // cors: true, // not needed for now
-  },
-);
+});
 
-export {
-  handler as GET,
-  handler as POST,
-  handler as PUT,
-  handler as PATCH,
-  handler as DELETE,
-  handler as OPTIONS,
-};
+export { handler as GET, handler as OPTIONS };
