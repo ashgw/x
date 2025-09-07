5d8dbd87d0bf30e3f0b452b5287ef6ce088b46f5
ashgw <ashrefgw@proton.me>
2025-09-07 06:37:04 +0100

    fix: log env checks

--- packages/observabiliy/src/log.ts
+++ packages/observabiliy/src/log.ts
@@ -1,14 +1,15 @@
-/* eslint-disable */
 import type { NextConfig } from "next";
 import { withLogtail } from "@logtail/next";
 
 import { env } from "@ashgw/env";
 
-// add OT if shit get serious
+// eslint-disable-next-line no-restricted-syntax
 const thirdParty = console; // would use logtail, but no need for now
-const defaultLogger = console;
+
+// eslint-disable-next-line no-restricted-syntax
+const localLogger = console;
 export const logger =
-  env.CURRENT_ENV === "production" ? thirdParty : defaultLogger;
+  env.CURRENT_ENV === "production" ? thirdParty : localLogger;
 
 // not used rn, use this with `next.js` config
 export const withLogging = (config: NextConfig): NextConfig => {
