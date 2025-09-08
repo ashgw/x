79da894107f71175219c924c8e6290d148f725e9
ashgw <ashrefgw@proton.me>
2025-09-08 01:00:31 +0100

    chore: remove timed

--- apps/www/src/api/router.ts
+++ apps/www/src/api/router.ts
@@ -1,79 +1,67 @@
 import { tsr } from "@ts-rest/serverless/fetch";
 import { contract } from "~/api/contract";
 import { fetchTextFromUpstream } from "./functions/fetchTextFromUpstream";
-import { timed } from "./timed";
 import { healthCheck } from "./functions/healthCheck";
 import { gpg } from "@ashgw/constants";
 import { webhooks } from "./functions/webhooks";
 
 export const router = tsr.router(contract, {
   bootstrap: async ({ query }) =>
-    timed("bootstrap", () =>
-      fetchTextFromUpstream({
-        query,
-        fetchUrl: {
-          github: {
-            repo: "dotfiles",
-            scriptPath: "install/bootstrap",
-          },
+    fetchTextFromUpstream({
+      query,
+      fetchUrl: {
+        github: {
+          repo: "dotfiles",
+          scriptPath: "install/bootstrap",
         },
-        opts: {
-          defaultRevalidate: 3600,
-          cacheControl: "s-maxage=3600, stale-while-revalidate=300",
-        },
-      }),
-    ),
-
+      },
+      opts: {
+        defaultRevalidate: 3600,
+        cacheControl: "s-maxage=3600, stale-while-revalidate=300",
+      },
+    }),
   debion: async ({ query }) =>
-    timed("debion", () =>
-      fetchTextFromUpstream({
-        query,
-        fetchUrl: {
-          github: {
-            repo: "debion",
-            scriptPath: "setup",
-          },
+    fetchTextFromUpstream({
+      query,
+      fetchUrl: {
+        github: {
+          repo: "debion",
+          scriptPath: "setup",
         },
-        opts: {
-          defaultRevalidate: 3600,
-          cacheControl: "s-maxage=3600, stale-while-revalidate=300",
-        },
-      }),
-    ),
+      },
+      opts: {
+        defaultRevalidate: 3600,
+        cacheControl: "s-maxage=3600, stale-while-revalidate=300",
+      },
+    }),
   whisper: async ({ query }) =>
-    timed("whisper", () =>
-      fetchTextFromUpstream({
-        query,
-        fetchUrl: {
-          github: {
-            repo: "whisper",
-            scriptPath: "setup",
-          },
+    fetchTextFromUpstream({
+      query,
+      fetchUrl: {
+        github: {
+          repo: "whisper",
+          scriptPath: "setup",
         },
-        opts: {
-          defaultRevalidate: 3600,
-          cacheControl: "s-maxage=3600, stale-while-revalidate=300",
-        },
-      }),
-    ),
+      },
+      opts: {
+        defaultRevalidate: 3600,
+        cacheControl: "s-maxage=3600, stale-while-revalidate=300",
+      },
+    }),
   gpg: async ({ query }) =>
-    timed("gpg", () =>
-      fetchTextFromUpstream({
-        query,
-        fetchUrl: {
-          direct: {
-            url: gpg.publicUrl,
-          },
+    fetchTextFromUpstream({
+      query,
+      fetchUrl: {
+        direct: {
+          url: gpg.publicUrl,
         },
-        opts: {
-          defaultRevalidate: 86400,
-          cacheControl: "s-maxage=86400, stale-while-revalidate=86400",
-        },
-      }),
-    ),
-  healthCheck: async () => timed("healthCheck", () => healthCheck()),
+      },
+      opts: {
+        defaultRevalidate: 86400,
+        cacheControl: "s-maxage=86400, stale-while-revalidate=86400",
+      },
+    }),
+  healthCheck: async () => healthCheck(),
   purgeViewWindow: async ({ headers }) =>
-    timed("purgeViewWindow", () =>
-      webhooks.purgeViewWindow({ "x-cron-token": headers["x-cron-token"] }),
-    ),
+    webhooks.purgeViewWindow({ "x-cron-token": headers["x-cron-token"] }),
 });
