b044015fadfd9f79a1b3f5a5f55922a43c7effe5
ashgw <ashrefgw@proton.me>
2025-09-08 14:35:46 +0100

    chore: ok were up to some

--- apps/www/src/api/middlewares/withRateLimiter.ts
+++ apps/www/src/api/middlewares/withRateLimiter.ts
@@ -7,11 +7,32 @@ import type { Keys } from "ts-roids";
 import type { NextRequest } from "next/server";
 import { logger } from "@ashgw/observability";
 
-type MiddleWareRequest = TsRestRequest & TsrContext & TsrContextWithRateLimiter;
+interface RateLimiter {
+  put: () => unknown;
+  pop: () => unknown;
+}
 
-export function middleware(
-  fn: (req: MiddleWareRequest, res: { nextRequest: NextRequest }) => void,
-) {
+export interface UserCtx {
+  user: {
+    email: string;
+    name: string;
+  };
+}
+
+export interface TsrContextWithRateLimiter {
+  ctx: TsrContext["ctx"] & { rl: RateLimiter };
+}
+
+interface __MergeTsrContextWithLocal<Ctx> {
+  ctx: TsrContext["ctx"] & Ctx;
+}
+
+export function middleware<
+  LocalCtx extends object,
+  MiddleWareRequest = TsRestRequest &
+    TsrContext &
+    __MergeTsrContextWithLocal<LocalCtx>,
+>(fn: (req: MiddleWareRequest, res: { nextRequest: NextRequest }) => void) {
   return (req: MiddleWareRequest) => {
     fn(req, { nextRequest: req as unknown as NextRequest });
   };
@@ -20,7 +41,7 @@ export function middleware(
 export function createMiddleware<
   LocalCtx extends object,
   Route extends Contract[Keys<Contract>],
->({ route, ctxKey }: { route: Route; ctxKey: string }) {
+>({ route }: { route: Route }) {
   const build = tsr.routeWithMiddleware(route)<
     TsrContext,
     {
@@ -28,22 +49,13 @@ export function createMiddleware<
     }
   >;
   type BuildOpts = Parameters<typeof build>[0];
-
-  return (handler: BuildOpts["handler"]) => {
-    return {
+  type Middleware = Parameters<typeof build>[0]["middleware"];
+  return (handler: BuildOpts["handler"], middleware: Middleware) => {
+    return build({
       handler,
-      middleware: [(req) => (req.ctx.rl.pop = ["1"].pop)],
-    };
+      middleware,
+    });
   };
-}
-
-interface RateLimiter {
-  put: () => unknown;
-  pop: () => unknown;
-}
-
-export interface TsrContextWithRateLimiter {
-  ctx: TsrContext["ctx"] & { rl: RateLimiter };
 }
 
 // TODO: also abstract this a sa middleware builder basicaly that's too cool and easy
