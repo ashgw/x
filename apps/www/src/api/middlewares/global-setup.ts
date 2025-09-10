e6db7f6c2c178e3ede37f05838f641efc8c25a1e
ashgw <ashrefgw@proton.me>
2025-09-10 13:30:06 +0100

    chore: use new handler

--- apps/www/src/api/middlewares/global-setup.ts
+++ apps/www/src/api/middlewares/global-setup.ts
@@ -1,26 +1,22 @@
 import { db } from "@ashgw/db";
 import type { GlobalContext } from "../context";
 import { logger } from "@ashgw/observability";
-import type {
-  ResponseHandlerResponse,
-  ResponseHandlerResquest,
-} from "~/@ashgw/ts-rest/handler";
-import { createGlobalRequestMiddleware } from "~/@ashgw/ts-rest";
+import {
+  createGlobalRequestMiddleware,
+  responseHandlersFn,
+} from "~/@ashgw/ts-rest";
 
 export const setupRequestMiddleware = () =>
   createGlobalRequestMiddleware<GlobalContext>((request) => {
     request.ctx = { requestedAt: new Date(), db };
   });
 
-// TODO: fix this shit and make it createGlobalResponseMiddleware
-// reference the fn file for more info how
-export const setupResponseHandlers = (
-  _res: ResponseHandlerResponse,
-  req: ResponseHandlerResquest<GlobalContext>,
-) => {
-  logger.log(
-    "[REST] took",
-    new Date().getTime() - req.ctx.requestedAt.getTime(),
-    "ms",
-  );
-};
+export const setupResponseHandlers = responseHandlersFn<void, GlobalContext>(
+  (res, req) => {
+    logger.log(
+      "[REST] took",
+      new Date().getTime() - req.ctx.requestedAt.getTime(),
+      "ms",
+    );
+  },
+);
