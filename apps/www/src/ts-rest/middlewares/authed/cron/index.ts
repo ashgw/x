601c97ca3fcae79e7a2829cc77c1b64551fb432a
ashgw <ashrefgw@proton.me>
2025-09-11 19:56:19 +0100

    chore: add more schemas

--- apps/www/src/ts-rest/middlewares/authed/cron/index.ts
+++ apps/www/src/ts-rest/middlewares/authed/cron/index.ts
@@ -8,7 +8,6 @@ type CronAuthedContext = EmptyObject; //  nothing we need to pass down here real
 
 export function cornAuthed(): SequentialMiddlewareRo<CronAuthedContext> {
   const mw = middlewareFn<GlobalContext, CronAuthedContext>((req, _res) => {
-    // zod already parsed it since it's declared in the contract as a header so this we got here is correct
     if (req.headers.get("x-cron-token") !== env.X_CRON_TOKEN) {
       return middlewareResponse.error({
         status: 401,
