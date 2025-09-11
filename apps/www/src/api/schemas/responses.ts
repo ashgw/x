c407a8b01d3a8bec2eaf89c033a4f43ad7ae7767
ashgw <ashrefgw@proton.me>
2025-09-11 04:04:29 +0100

    chore: add schema error TODOs

--- apps/www/src/api/schemas/responses.ts
+++ apps/www/src/api/schemas/responses.ts
@@ -30,7 +30,59 @@ export const fetchGpgFromUpstreamSchemaResponses = createSchemaResponses({
 
 export const purgeViewWindowSchemaResponses = createSchemaResponses({
   200: c.noBody(),
-  401: httpErrorSchema,
+  401: httpErrorSchema, // TODO: here make to be createHttpErrorSchema.forbidden() or httpErrorSchema.unauthorized()
+  // // so it doesnt pollute the generated openAPI schema, since
+
+  /** 
+   * rn it looks like this 
+   * "403": {
+            "description": "403",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "object",
+                  "properties": {
+                    "code": {
+                      "type": "string",
+                      "enum": [
+                        "UPSTREAM_ERROR",
+                        "INTERNAL_ERROR",
+                        "BAD_REQUEST",
+                        "NOT_FOUND",
+                        "UNAUTHORIZED",
+                        "FORBIDDEN"
+                      ],
+                      "description": "Stable, machine-parseable error code"
+                    },
+                    "message": {
+                      "type": "string",
+                      "minLength": 1,
+                      "maxLength": 1000,
+                      "description": "Human readable"
+                    },
+                    "details": {
+                      "type": "object",
+                      "additionalProperties": {
+                        "nullable": true
+                      },
+                      "description": "Optional extra context"
+                    }
+                  },
+                  "required": [
+                    "code",
+                    "message"
+                  ]
+                }
+              }
+            }
+          },
+
+
+   * whoch os fickng annoying and big af, and the description doesnt make any sense really, like bro i should describe wtf 
+          and why, like for exmaple, 'not authorized to perform this action' or forbidden or whatever, we defailt to those, but
+          we let the user describe the schema too of the descripton, like httpErrorSchema.unauthorized().describe('blah blah...')
+   * 
+   */
   403: httpErrorSchema,
   500: httpErrorSchema,
 });
