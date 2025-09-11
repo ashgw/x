da9db7382a72183b5a24bdffd57caa86834cf509
ashgw <ashrefgw@proton.me>
2025-09-11 16:10:15 +0100

    fix: rl window

--- apps/www/src/ts-rest/middlewares/rateLimiter/window.ts
+++ apps/www/src/ts-rest/middlewares/rateLimiter/window.ts
@@ -1,15 +1,24 @@
 export type RlWindow = `${number}s` | `${number}h` | `${number}d`;
 
 export function parseWindow(window: RlWindow): number {
-  const unit = window.slice(-1) as "s" | "h" | "d";
-  const num = parseInt(window.slice(0, -1), 10);
-
-  if (Number.isNaN(num) || num <= 0) {
+  const m = /^(\d+)([shd])$/.exec(window);
+  if (!m) {
     throw new Error(`Invalid RlWindow value: ${window}`);
   }
 
-  if (unit === "s") return num * 1000;
-  if (unit === "h") return num * 60 * 60 * 1000;
-  // must be "d"
-  return num * 24 * 60 * 60 * 1000;
+  const num = Number(m[1]);
+  const unit = m[2] as "s" | "h" | "d";
+
+  if (num <= 0) {
+    throw new Error(`Invalid RlWindow value: ${window}`);
+  }
+
+  switch (unit) {
+    case "s":
+      return num * 1000;
+    case "h":
+      return num * 60 * 60 * 1000;
+    case "d":
+      return num * 24 * 60 * 60 * 1000;
+  }
 }
