d366d6ed7e9d4fc20fdf4cfdab0eb238b64280c5
ashgw <ashrefgw@proton.me>
2025-09-12 21:54:32 +0100

    docs: to client

--- apps/www/src/ts-rest/client.ts
+++ apps/www/src/ts-rest/client.ts
@@ -1,6 +1,3 @@
-// TODO: ask cursor to document all this shit
-// TODO: fix why we can't infer errros when using the lciet, the data is inferred correctly, but the erro isent, unlike
-// what happens in tRPC
 import { initTsrReactQuery } from "@ts-rest/react-query/v5";
 import { initClient } from "@ts-rest/core";
 import { contract } from "~/api/contract";
@@ -9,12 +6,21 @@ import type { InitClientArgs } from "@ts-rest/core";
 
 const args = {
   baseUrl: endPoint,
-  baseHeaders: {},
-  validateResponse: true,
-  throwOnUnknownStatus: true,
-  jsonQuery: false,
+  baseHeaders: {}, // can be used for auth, for exmaple, extract a CSRF token with a session cookie & send
+  validateResponse: true, // If true, validates responses against your schemas at runtime.
+  throwOnUnknownStatus: true, //  If true, throws if server returns a status not declared in your contract.
+  jsonQuery: false, //  If true, encodes query params as JSON instead of standard URL encoding.
 } satisfies InitClientArgs;
 
 export const sdk = initClient(contract, args);
 
+/**
+ * React Query integration for ts-rest.
+ *
+ * Provides auto-generated hooks like:
+ * ```ts
+ * const { data, error } = tsrQueryClientSide.healthCheck.useQuery(["hc"]);
+ * ```
+ *
+ */
 export const tsrQueryClientSide = initTsrReactQuery(contract, args);
--- apps/www/src/ts-rest/context.ts
+++ apps/www/src/ts-rest/context.ts
@@ -2,9 +2,6 @@ import type { DatabaseClient } from "@ashgw/db";
 import type { GlobalTsrContext } from "~/@ashgw/ts-rest";
 
 export interface GlobalContext extends GlobalTsrContext {
-  // TODO: tell the user that it must  be extend like this
-  /*
-   */
   ctx: {
     requestedAt: Date;
     db: DatabaseClient;
