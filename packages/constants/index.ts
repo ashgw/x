b8d99461ff4dbb494fc921c3b03a8c90d2a0850c
ashgw <ashrefgw@proton.me>
2025-09-13 10:11:14 +0100

    fix: consts

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,7 +1,7 @@
 export const email = "oss@ashgw.me";
 export const repo_source = "https://github.com/ashgw/x";
-export const creator = "AG";
-export const site_name = creator;
+export const creator = "@rccyx";
+export const site_name = "AG";
 
 export const links = {
   keyBase: "https://keybase.io/ashgw",
