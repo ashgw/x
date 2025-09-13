8febde960d23910635f9b2cec449e7ad70531ee8
ashgw <oss@ashgw.me>
2025-09-13 17:37:40 -0500

    fix(ci): serverless (#613)
    * chore: fix dev deps
    
    * ci: fix prod deploys (serverless)
    
    * fix: openapi
    
    * fix(build): deps
    
    * chore: update lock
    
    * fix: add missing operationID
    
    * fix: add missing operationID II
    
    * fix: generated client problem
    
    * fix: generated client problem II

--- apps/www/src/app/api/v1/openapi/route.ts
+++ apps/www/src/app/api/v1/openapi/route.ts
@@ -5,18 +5,25 @@ import { endPoint } from "~/ts-rest/endpoint";
 import { env } from "@ashgw/env";
 
 export const runtime = "edge";
-
 export const revalidate = 3600;
 
 export function GET() {
-  const doc = generateOpenApi(contract, {
-    info: {
-      title: "www API v1",
-      version: "1.0.0",
-      description: "REST",
+  const doc = generateOpenApi(
+    contract,
+    {
+      info: {
+        title: "www API v1",
+        version: "1.0.0",
+        description: "REST",
+        contact: { email: "oss@ashgw.me" },
+      },
+      openapi: "3.1.0",
+      servers: [{ url: new URL(endPoint, env.NEXT_PUBLIC_WWW_URL).toString() }],
     },
-    openapi: "3.0.3",
-    servers: [{ url: new URL(endPoint, env.NEXT_PUBLIC_WWW_URL).toString() }],
-  });
+    {
+      setOperationId: true,
+    },
+  );
+
   return NextResponse.json(doc, { status: 200 });
 }
