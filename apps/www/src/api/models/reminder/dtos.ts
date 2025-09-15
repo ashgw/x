741b1eb76badb4b731b6e31029081373ca0e88f1
ashgw <ashrefgw@proton.me>
2025-09-15 18:34:00 +0100

    docs: to schemas

--- apps/www/src/api/models/reminder/dtos.ts
+++ apps/www/src/api/models/reminder/dtos.ts
@@ -7,40 +7,43 @@ const isoDateTimeSchema = z
   .datetime({ offset: true })
   .describe("e.g. '2025-01-01T00:00:00+00:00'");
 
-export const reminderPayloadSchemaDto = z.object({
+const scheduleAtSchema = z.object({
+  kind: z.literal("at").describe("At a specific date and time"),
+  at: isoDateTimeSchema,
   notification: notifyBodySchemaDto,
 });
 
-const scheduleAtSchema = reminderPayloadSchemaDto.extend({
-  kind: z.literal("at"),
-  at: isoDateTimeSchema,
-});
-
-const scheduleCronSchema = reminderPayloadSchemaDto.extend({
-  kind: z.literal("cron"),
+const scheduleCronSchema = z.object({
+  kind: z.literal("cron").describe("At a specific date and time"),
   cron: z.object({
     timezone: z.string().min(1).max(128).describe("e.g. 'America/New_York'"),
     expression: z.string().min(1).max(16).describe("e.g. '0 0 * * *'"),
   }),
+  notification: notifyBodySchemaDto,
 });
 
 const scheduleMultiAtSchema = z.object({
-  kind: z.literal("multiAt").describe("At multiple specific date and times"),
-  at: z.array(isoDateTimeSchema).min(1).max(10),
+  kind: z.literal("multiAt").describe("At multiple specific dates and times"),
   notifications: z
-    .array(notifyBodySchemaDto)
-    .min(1)
+    .array(
+      z.object({
+        at: isoDateTimeSchema,
+        notification: notifyBodySchemaDto,
+      }),
+    )
     .describe(
       "Notifications to send at each date and time, sequentially, at least one notification is required",
     ),
 });
 
-export const createReminderBodySchemaDto = z.object({
-  schedule: z.discriminatedUnion("kind", [
-    scheduleAtSchema,
-    scheduleCronSchema,
-    scheduleMultiAtSchema,
-  ]),
-});
+export const createReminderBodySchemaDto = z
+  .object({
+    schedule: z.discriminatedUnion("kind", [
+      scheduleAtSchema,
+      scheduleCronSchema,
+      scheduleMultiAtSchema,
+    ]),
+  })
+  .describe("The reminder to create.");
 
 export type CreateReminderBodyDto = z.infer<typeof createReminderBodySchemaDto>;
