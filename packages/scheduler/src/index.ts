624d42b14af8b3a6ddd1dd604a8dd2fb78731a29
ashgw <ashrefgw@proton.me>
2025-09-15 23:22:31 +0100

    build: add scheduler package

---
+++ packages/scheduler/src/index.ts
@@ -0,0 +1,77 @@
+import { Client as QstashClient } from "@upstash/qstash";
+import { env } from "@ashgw/env";
+import type {
+  Payload,
+  ScheduleDto,
+  AtDto,
+  CronDto,
+  ScheduleAtResult,
+  ScheduleCronResult,
+} from "./types";
+
+const qstashClient = new QstashClient({ token: env.QSTASH_TOKEN });
+
+class SchedulerService {
+  constructor(
+    private readonly _headers: Record<string, string> = {
+      "Content-Type": "application/json",
+    },
+  ) {}
+
+  public headers(headers: Record<string, string>): SchedulerService {
+    return new SchedulerService({ ...this._headers, ...headers });
+  }
+
+  public async schedule(input: AtDto): Promise<ScheduleAtResult>;
+  public async schedule(input: CronDto): Promise<ScheduleCronResult>;
+  public async schedule(
+    input: ScheduleDto,
+  ): Promise<ScheduleAtResult | ScheduleCronResult> {
+    if ("at" in input) {
+      return this.scheduleAt({
+        atTime: input.at.datetimeIso,
+        url: input.url,
+        payload: input.payload,
+      });
+    }
+    return this.scheduleCron({
+      expression: input.cron.expression,
+      url: input.url,
+      payload: input.payload,
+    });
+  }
+
+  private async scheduleAt(input: {
+    url: string;
+    payload: Payload;
+    atTime: string;
+  }): Promise<ScheduleAtResult> {
+    const response = await qstashClient.publish({
+      url: input.url,
+      body: input.payload,
+      headers: this._headers,
+      notBefore: SchedulerService._toUnixSecond(input.atTime),
+    });
+    return { messageId: response.messageId };
+  }
+
+  private async scheduleCron(input: {
+    url: string;
+    payload: Payload;
+    expression: string;
+  }): Promise<ScheduleCronResult> {
+    const response = await qstashClient.schedules.create({
+      destination: input.url,
+      cron: input.expression,
+      body: input.payload,
+      headers: this._headers,
+    });
+    return { scheduleId: response.scheduleId };
+  }
+
+  private static _toUnixSecond(isoString: string): number {
+    return Math.floor(new Date(isoString).getTime() / 1000);
+  }
+}
+
+export const scheduler = new SchedulerService();
