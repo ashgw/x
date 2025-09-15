48886855b8b6c30da3d267f6cf15171200a2eccf
ashgw <ashrefgw@proton.me>
2025-09-15 14:05:32 +0100

    chore: update contract

--- apps/www/src/api/functions/webhooks/index.ts
+++
@@ -1,9 +0,0 @@
-import { purgeViewWindow } from "./purgeViewWindow";
-import { purgeTrashPosts } from "./purgeTrashPosts";
-import { notify } from "./notify";
-
-export const webhooks = {
-  purgeViewWindow,
-  purgeTrashPosts,
-  notify,
-} as const;
