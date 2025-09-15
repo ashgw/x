0171774f664ee4e42cd5b274a38d5f750928e817
ashgw <ashrefgw@proton.me>
2025-09-15 22:09:06 +0100

    chore: type shenanigans

--- packages/scheduler/src/index.ts
+++ packages/scheduler/src/index.ts
@@ -1,6 +1,7 @@
 import { Client as QstashClient } from "@upstash/qstash";
 import { env } from "@ashgw/env";
-import type { ExclusiveUnnion } from "ts-roids";
+import type { ExclusiveUnion } from "ts-roids";
+
 const qstashClient = new QstashClient({ token: env.QSTASH_TOKEN });
 
 type Payload =
@@ -13,11 +14,41 @@ type Payload =
 interface ScheduleRo {
   id: string;
 }
+
+interface ScheduleBaseDto {
+  url: string;
+  payload: Payload;
+}
+
+type ScheduleType = ExclusiveUnion<
+  | {
+      atTime: {
+        datetimeIso: string;
+      };
+    }
+  | { cron: { expression: string } }
+>;
+
+type ScheduleDto = ScheduleBaseDto & ScheduleType;
+
 export class SchedulerService {
   private static _headers = {
     "Content-Type": "application/json",
     "x-api-token": env.X_API_TOKEN,
   } as const;
+
+  public async schedule(input: ScheduleDto): Promise<ScheduleRo> {
+    if (input.atTime) {
+      return this.scheduleAt({
+        at: input.atTime.datetimeIso,
+        ...input,
+      });
+    }
+    return this.scheduleCron({
+      expression: input.cron.expression,
+      ...input,
+    });
+  }
 
   public async scheduleAt(input: {
     url: string;
@@ -38,11 +69,11 @@ export class SchedulerService {
   public async scheduleCron(input: {
     url: string;
     payload: Payload;
-    cron: string;
+    expression: string;
   }): Promise<ScheduleRo> {
     const response = await qstashClient.schedules.create({
       destination: input.url,
-      cron: input.cron,
+      cron: input.expression,
       body: input.payload,
       headers: SchedulerService._headers,
     });
