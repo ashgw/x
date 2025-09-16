43d36fccefd4d831a4aae42b1b398ecf5135642a
ashgw <ashrefgw@proton.me>
2025-09-17 00:10:17 +0100

    feat: add endpoints

---
+++ apps/www/src/api/endpoints.ts
@@ -0,0 +1,11 @@
+export const endpoints = {
+  reminder: "/reminder",
+  notify: "/notify",
+  purgeViewWindow: "/purge-view-window",
+  purgeTrashPosts: "/purge-trash-posts",
+  healthCheck: "/health-check",
+  gpg: "/gpg",
+  bootstrap: "/bootstrap",
+  debion: "/debion",
+  whisper: "/whisper",
+};
