c64ce914aa7a0cff9f9f60a258395f9ec17ddab1
ashgw <ashrefgw@proton.me>
2025-09-21 00:42:48 +0100

    feat: finish biz logic

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -1,32 +1,9 @@
-const ERROR_CODES = {
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-  FORBIDDEN: 403,
-  NOT_FOUND: 404,
-  CONFLICT: 409,
-  INTERNAL_SERVER_ERROR: 500,
-  NOT_IMPLEMENTED: 501,
-} as const;
-
-type ErrorCode = keyof typeof ERROR_CODES;
-
-interface ErrorOptions {
-  code: ErrorCode;
-  message: string;
-  cause?: unknown;
-}
-
-export class InternalError extends Error {
-  public readonly code: ErrorCode;
-  public readonly statusCode: number;
-  public override readonly cause?: unknown;
-
-  constructor({ code, message, cause }: ErrorOptions) {
-    super(message);
-    this.name = "InternalError";
-    this.code = code;
-    this.statusCode = ERROR_CODES[code];
-    this.cause = cause;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
+export { AppError } from "./error";
+export type { AppErrorOptions } from "./error";
+export { AppCodes } from "./codes";
+export type { AppCode, AppHttpBody } from "./codes";
+export { isAppError, ensureAppError } from "./guards";
+export { httpStatusFromCode, toHttp, toHttpFromUnknown } from "./http";
+export type { HttpStatus } from "./http";
+export { trpcCodeFromApp, toTrpc, toTrpcFromUnknown } from "./trpc";
+export { E } from "./factory";
