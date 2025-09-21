c29c5bf0485ad20d31c8041ebe1c69760505840b
ashgw <ashrefgw@proton.me>
2025-09-21 02:17:06 +0100

    fix: logtail token env

--- packages/logger/src/env.ts
+++ packages/logger/src/env.ts
@@ -5,5 +5,4 @@ export function detectRuntimeEnv(): RuntimeEnv {
   return env.NEXT_PUBLIC_CURRENT_ENV;
 }
 
-export const LOGTAIL_TOKEN: string | undefined =
-  env.NEXT_PUBLIC_LOGTAIL_INGESTION_TOKEN;
+export const LOGTAIL_TOKEN = env.NEXT_PUBLIC_LOGTAIL_INGESTION_TOKEN;
