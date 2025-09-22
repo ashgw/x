627b028528d8358d1e16f80d6bc9398fd6bb0e17
ashgw <ashrefgw@proton.me>
2025-09-22 23:44:57 +0100

    fix: untrack embedded ts-rest-kit tree

--- apps/www/src/ts-rest-kit/index.ts
+++
@@ -1,6 +0,0 @@
-export type { GlobalTsrContext } from "./ctx";
-
-export * from "./inference";
-export { createContract } from "./contract";
-export * from "./middleware";
-export * from "./schemas";
