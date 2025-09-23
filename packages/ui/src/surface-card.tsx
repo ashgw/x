e82a092c7f1f3dc97c7c7f5676ebc2567e89e6a1
ashgw <ashrefgw@proton.me>
2025-09-23 19:32:24 +0100

    feat: add surface card

---
+++ packages/ui/src/surface-card.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import type { VariantProps } from "@ashgw/ui";
+import { cva, cn } from "@ashgw/ui";
+
+const surfaceCardVariants = cva(
+  "group flex flex-col border border-white/10 shadow transition-all", // base
+  {
+    variants: {
+      variant: {
+        default: "bg-background/50",
+        glow: "glowsup-dimmed hover:slower-translate hover:shadow-[0px_4px_88px_0px_var(--deeper-purple)]",
+        subtle: "hover:shadow-md hover:border-white/20",
+        flat: "border-none shadow-none bg-transparent",
+      },
+      size: {
+        default: "p-5 rounded-[2rem]",
+        sm: "p-3 rounded-xl",
+        lg: "p-8 rounded-[2.5rem]",
+      },
+      animate: {
+        none: "",
+        scale: "hover:scale-105 slower-transition",
+        pop: "hover:scale-110 slower-transition",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+      animate: "scale",
+    },
+  },
+);
+
+export interface CardProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof surfaceCardVariants> {
+  asChild?: boolean;
+}
+
+export const SurfaceCard = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, variant, size, animate, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? "section" : "div";
+    return (
+      <Comp
+        ref={ref}
+        className={cn(
+          surfaceCardVariants({ variant, size, animate, className }),
+        )}
+        {...props}
+      />
+    );
+  },
+);
+SurfaceCard.displayName = "SurfaceCard";
