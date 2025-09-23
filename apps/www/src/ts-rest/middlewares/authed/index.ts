93621d3868e59d1caa0b88dc1e110098f60785d0
ashgw <oss@ashgw.me>
2025-09-23 03:28:12 -0500

    chore: remove clunk (#634)

--- apps/www/src/ts-rest/middlewares/authed/index.ts
+++ apps/www/src/ts-rest/middlewares/authed/index.ts
@@ -1,28 +1,14 @@
 import { env } from "@ashgw/env";
+import type { EmptyObject } from "ts-roids";
 import { middlewareResponse, middlewareFn } from "~/ts-rest-kit";
 import type { GlobalContext } from "~/ts-rest/context";
 
-interface AuthedCtx {
-  user: {
-    email: string;
-    name: string;
-  };
-}
-
 export function authed() {
-  const user = {
-    email: "admin@email.com",
-    name: "admin",
-  } as const;
-
-  return middlewareFn<GlobalContext, AuthedCtx>((req, _res) => {
+  return middlewareFn<GlobalContext, EmptyObject>((req, _res) => {
     if (req.headers.get("x-api-token") !== env.X_API_TOKEN) {
       return middlewareResponse.errors.unauthorized({
         message: "Invalid token. You cannot perform this action",
       });
     }
-    return {
-      ctx: { user },
-    };
   });
 }
