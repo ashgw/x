b14e8e025b1bd6a6b0781d2426315940eb42a2fc
ashgw <ashrefgw@proton.me>
2025-09-25 21:16:20 +0100

    fix: icon

--- apps/blog/src/app/components/pages/editor/components/header/components/ProfileButton.tsx
+++ apps/blog/src/app/components/pages/editor/components/header/components/ProfileButton.tsx
@@ -1,26 +1,15 @@
 import Link from "next/link";
-import { motion } from "framer-motion";
 import { User } from "lucide-react";
 
 import { Button } from "@ashgw/ui";
 
 export function ProfileButton() {
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.8 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 0.4,
-        delay: 0.1,
-      }}
-      whileHover={{ scale: 1.05 }}
-    >
-      <Button variant="outline" className="h-9 w-9" asChild>
-        <Link href="/profile">
-          <User className="h-4 w-4" />
-          <span className="sr-only">Go to profile</span>
-        </Link>
-      </Button>
-    </motion.div>
+    <Button variant="outline">
+      <Link href="/profile">
+        <User className="h-5 w-5" />
+        <span className="sr-only">Go to profile</span>
+      </Link>
+    </Button>
   );
 }
