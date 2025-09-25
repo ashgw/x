87819f84a49a93baa632137334fac24d0bcfaace
ashgw <ashrefgw@proton.me>
2025-09-25 20:32:20 +0100

    chore: clearoff

--- apps/blog/src/app/components/pages/[post]/components/newsletter/index.tsx
+++ apps/blog/src/app/components/pages/[post]/components/newsletter/index.tsx
@@ -75,6 +75,7 @@ export function Newsletter() {
                   <FormControl>
                     <div className="flex w-full items-center gap-3">
                       <Input
+                        variant="rounded"
                         type="email"
                         {...field}
                         placeholder="your@email.com"
