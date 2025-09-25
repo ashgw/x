8918da3dd7df7fc3867df331a3de0c67e3529939
ashgw <ashrefgw@proton.me>
2025-09-25 12:19:34 +0100

    chore: workable prior to override

--- packages/design/src/tailwind/preset.ts
+++ packages/design/src/tailwind/preset.ts
@@ -9,7 +9,11 @@ export const designPreset: Config = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1400px", // override from default tailwind
       },
     },
     extend: {
