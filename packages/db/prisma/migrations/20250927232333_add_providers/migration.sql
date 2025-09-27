614cee891bcbd07c337be0856f452decb36c2473
ashgw <ashrefgw@proton.me>
2025-09-28 00:32:35 +0100

    chore: add user seeder

--- packages/db/prisma/migrations/20250927232333_add_providers/migration.sql
+++ packages/db/prisma/migrations/20250927232333_add_providers/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - You are about to drop the column `passwordHash` on the `User` table. All the data in the column will be lost.
-  - You are about to drop the column `role` on the `User` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[token]` on the table `Session` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `token` to the `Session` table without a default value. This is not possible if the table is not empty.
   - Added the required column `updatedAt` to the `Session` table without a default value. This is not possible if the table is not empty.
@@ -20,7 +19,6 @@ ALTER COLUMN "userId" SET DATA TYPE TEXT;
 
 -- AlterTable
 ALTER TABLE "User" DROP COLUMN "passwordHash",
-DROP COLUMN "role",
 ADD COLUMN     "emailVerified" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "image" TEXT,
 ALTER COLUMN "email" SET DATA TYPE TEXT,
