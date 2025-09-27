f59fda3f4b25d7159f327927bcc1fb56986faf5d
ashgw <ashrefgw@proton.me>
2025-09-27 18:31:30 +0100

    chore: do errors

--- apps/blog/src/api/services/auth/better/auth.ts
+++ apps/blog/src/api/services/auth/better/auth.ts
@@ -4,11 +4,14 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { env } from "@ashgw/env";
 import { siteName } from "@ashgw/constants";
 import argon2 from "argon2";
+import { authEndpoints } from "./endpoints";
+import {AppError} from '@ashgw/error'
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql",
   }),
+  basePath: authEndpoints.basePath,
   baseURL: env.NEXT_PUBLIC_BLOG_URL,
   session: {
     expiresIn: 60 * 60 * 24 * 14, // 14 days (default is 7)
@@ -54,6 +57,16 @@ export const auth = betterAuth({
           ? logger.warn(message)
           : logger.error(message);
     },
+  },
+  onAPIError: {
+    errorURL: authEndpoints.error,
+    throw: false, // I'll handle it so it comes typed
+    onError: async (error, _authCtx) => {
+      const err = AppError.fromUnknown(error)                                       
+      logger.error(
+        `Error in auth API route: ${err.message}`,
+      );
+      await Promise.resolve();
   },
   socialProviders: {
     google: {
