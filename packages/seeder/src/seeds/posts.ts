fcb6324b599b98d1685e737931508259ba2b5b7f
ashgw <ashrefgw@proton.me>
2025-09-28 11:56:34 +0100

    fix: seeder logic

---
+++ packages/seeder/src/seeds/posts.ts
@@ -0,0 +1,35 @@
+import { db } from "@ashgw/db";
+import { blogs } from "./../data/mdx";
+
+export async function seedPosts() {
+  for (const blog of blogs) {
+    await db.post.upsert({
+      where: { slug: blog.slug },
+      update: {
+        title: blog.title,
+        summary: blog.summary,
+        isReleased: blog.isReleased,
+        firstModDate: blog.firstModDate,
+        lastModDate: blog.lastModDate,
+        minutesToRead: blog.minutesToRead,
+        tags: blog.tags,
+        category: blog.category,
+        mdxText: blog.mdxContentRaw,
+      },
+      create: {
+        slug: blog.slug,
+        title: blog.title,
+        summary: blog.summary,
+        isReleased: blog.isReleased,
+        firstModDate: blog.firstModDate,
+        lastModDate: blog.lastModDate,
+        minutesToRead: blog.minutesToRead,
+        tags: blog.tags,
+        category: blog.category,
+        mdxText: blog.mdxContentRaw,
+      },
+    });
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(`[seed] upserted post: ${blog.slug}`);
+  }
+}
