3179dbdecfc7d55a23249db7b3a45202aa26d9f2
ashgw <ashrefgw@proton.me>
2025-09-28 16:12:02 +0100

    chore: add session mappers

--- apps/blog/src/api/mappers/session.ts
+++ apps/blog/src/api/mappers/session.ts
@@ -1,85 +1,15 @@
-import type { PostCategory as DbPostCategory } from "@ashgw/db/raw";
+import type { SessionRo } from "../models";
+import type { SessionAuthQuery } from "../query-helpers/session";
 
-import type {
-  fontMatterMdxContentRo,
-  PostCardRo,
-  PostArticleRo,
-  TrashPostArticleRo,
-} from "../models";
-import type {
-  PostCardQuery,
-  PostArticleQuery,
-  TrashPostArticleQuery,
-} from "../query-helpers";
-import { PostCategoryEnum } from "../models";
-
-export class PostMapper {
-  public static toCardRo({ post }: { post: PostCardQuery }): PostCardRo {
+export class SessionMapper {
+  public static toRo({ session }: { session: SessionAuthQuery }): SessionRo {
     return {
-      slug: post.slug,
-      title: post.title,
-      summary: post.summary,
-      firstModDate: post.firstModDate,
-      minutesToRead: post.minutesToRead,
-      tags: post.tags,
-      views: post.viewsCount,
-      category: this._mapCategory({
-        category: post.category,
-      }),
+      createdAt: session.createdAt,
+      expiresAt: session.expiresAt,
+      updatedAt: session.updatedAt,
+      token: session.token,
+      userAgent: session.userAgent ? session.userAgent : undefined,
+      userId: session.userId,
     };
-  }
-
-  public static toArticleRo({
-    post,
-  }: {
-    post: PostArticleQuery;
-    fontMatterMdxContent: fontMatterMdxContentRo;
-  }): PostArticleRo {
-    return {
-      ...this.toCardRo({ post }),
-      lastModDate: post.lastModDate,
-      isReleased: post.isReleased,
-      slug: post.slug,
-      fontMatterMdxContent: {
-        body: post.mdxText,
-        bodyBegin: 0,
-      },
-    };
-  }
-  public static toTrashRo({
-    post,
-  }: {
-    post: TrashPostArticleQuery;
-  }): TrashPostArticleRo {
-    return {
-      category: this._mapCategory({
-        category: post.category,
-      }),
-      title: post.title,
-      summary: post.summary,
-      tags: post.tags,
-      mdxText: post.mdxText,
-      deletedAt: post.deletedAt,
-      originalSlug: post.originalSlug,
-      firstModDate: post.firstModDate,
-      lastModDate: post.lastModDate,
-      wasReleased: post.wasReleased,
-      id: post.id,
-    };
-  }
-
-  private static _mapCategory({
-    category,
-  }: {
-    category: DbPostCategory;
-  }): PostCategoryEnum {
-    switch (category) {
-      case "HEALTH":
-        return PostCategoryEnum.HEALTH;
-      case "PHILOSOPHY":
-        return PostCategoryEnum.PHILOSOPHY;
-      case "SOFTWARE":
-        return PostCategoryEnum.SOFTWARE;
-    }
   }
 }
--- apps/blog/src/api/models/session/index.ts
+++ apps/blog/src/api/models/session/index.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 import { id, token } from "../shared";
 
 export const sessionSchemaRo = z.object({
-  id,
+  // no need for ID
   createdAt: z.date(),
   userId: id,
   token,
   updatedAt: z.date(),
   expiresAt: z.date(),
-  userAgent: z.string().min(1).max(2048),
+  userAgent: z.string().min(1).max(2048).optional(),
 });
 
 export type SessionRo = z.infer<typeof sessionSchemaRo>;
