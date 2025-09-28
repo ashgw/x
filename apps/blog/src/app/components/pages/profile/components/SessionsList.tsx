9df8c4094bad3d5143ee930b6f4794cfb262e3fb
ashgw <ashrefgw@proton.me>
2025-09-28 17:02:50 +0100

    chore: wip

--- apps/blog/src/app/components/pages/profile/components/SessionsList.tsx
+++ apps/blog/src/app/components/pages/profile/components/SessionsList.tsx
@@ -180,7 +180,7 @@ export function SessionsList() {
                         variant="destructive"
                         onClick={() =>
                           terminateSpecificSessionMutation.mutate({
-                            sessionId: session.id,
+                            token: ...
                           })
                         }
                         disabled={isLoading}
