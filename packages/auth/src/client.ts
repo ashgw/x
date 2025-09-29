edc1c2cfc7d8308c7499763883f002ee60cfe16d
ashgw <ashrefgw@proton.me>
2025-09-29 03:34:17 +0100

    chore: remove client

--- packages/auth/src/client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from "better-auth/client";
-import { env } from "@ashgw/env";
-import { authEndpoints } from "./consts";
-
-// TODO: remove this &    remove the endpoint too
-export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_BLOG_URL + authEndpoints.basePath,
-});
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,3 +1,2 @@
 export { nextJsHandler } from "./next-handler";
-export { authClient } from "./client";
 export { auth } from "./server";
