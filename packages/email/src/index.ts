ecf8866f22a1c8fa5f2b963f7634d863c22e6c36
ashgw <ashrefgw@proton.me>
2025-09-29 02:26:55 +0100

    fix: email package exports

--- packages/email/src/index.ts
+++ packages/email/src/index.ts
@@ -1,13 +1,2 @@
+export { send } from "./senders";
 export { NotificationType } from "./types";
-export {
-  email,
-  EmailSenders,
-  sendVerifyEmail,
-  sendEmailIsVerified,
-  sendResetPassword,
-  sendAccountDeleted,
-  type VerifyEmailParams,
-  type EmailIsVerifiedParams,
-  type ResetPasswordParams,
-  type AccountDeletedParams,
-} from "./senders";
