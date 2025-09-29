6671ef2fe85d4677ebe5e35062fe1e998ba53a7d
ashgw <ashrefgw@proton.me>
2025-09-29 02:34:18 +0100

    fix: lint issues

--- packages/email/src/test.ts
+++ packages/email/src/test.ts
@@ -1,12 +1,12 @@
 import { env } from "@ashgw/env";
-import { email } from "./index";
+import { send } from "./index";
 import { logger } from "@ashgw/logger";
 
 async function main(): Promise<void> {
-  await email.sendVerifyEmail({
+  await send.auth.verifyEmail({
     to: env.PERSONAL_EMAIL,
     verifyUrl: `${env.NEXT_PUBLIC_BLOG_URL}/verify?token=example`,
-    userName: "AG",
+    userName: "John Doe",
   });
   logger.info("Sent verify email");
 }
