1db5f07426251b9a41498c3576531068962c6927
ashgw <ashrefgw@proton.me>
2025-09-29 04:36:03 +0100

    fix: lint

--- apps/blog/src/trpc/middlewares/auth/authentication.ts
+++ apps/blog/src/trpc/middlewares/auth/authentication.ts
@@ -2,12 +2,12 @@ import { TRPCError } from "@trpc/server";
 
 import type { UserRo } from "~/api/models";
 import type { TrpcContext } from "~/trpc/context";
-import { AuthService } from "~/api/services";
+import { UserService } from "~/api/services";
 
 export async function isAuthenticated(input: {
   ctx: TrpcContext;
 }): Promise<UserRo> {
-  const user = await new AuthService({ ctx: input.ctx }).me();
+  const user = await new UserService({ ctx: input.ctx }).me();
 
   if (!user) {
     throw new TRPCError({
