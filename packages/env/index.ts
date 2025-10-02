840fc5f7e5da0d408223e1145012eabe6e33587a
ashgw <ashrefgw@proton.me>
2025-10-02 01:26:00 +0100

    feat: add ci vars

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -2,18 +2,21 @@ import { createEnv } from "@ashgw/ts-env";
 import { colors } from "./colors";
 
 import { envTuple } from "./env-tuple";
-import { clientVars, serverVars } from "./vars";
+import { ciVars, clientVars, serverVars } from "./vars";
 
 const serverVarsTuple = envTuple(serverVars);
+const ciVarsTuple = envTuple(ciVars);
 
 export const env = createEnv({
   vars: {
-    ...clientVars,
     ...serverVars,
+    ...ciVars,
+    ...clientVars,
   },
-  disablePrefix: [...serverVarsTuple],
+  disablePrefix: [...serverVarsTuple, ...ciVarsTuple],
   prefix: "NEXT_PUBLIC",
   runtimeEnv: {
+    // server vars
     QSTASH_TOKEN: process.env.QSTASH_TOKEN,
     X_API_TOKEN: process.env.X_API_TOKEN,
     PERSONAL_EMAIL: process.env.PERSONAL_EMAIL,
@@ -32,6 +35,22 @@ export const env = createEnv({
     SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
     SENTRY_PROJECT: process.env.SENTRY_PROJECT,
     AUTH_ENCRYPTION_KEY: process.env.AUTH_ENCRYPTION_KEY,
+    // ci vars
+    TURBO_TOKEN: process.env.TURBO_TOKEN,
+    TURBO_TEAM: process.env.TURBO_TEAM,
+    VERCEL_TOKEN: process.env.VERCEL_TOKEN,
+    VERCEL_ORG_ID: process.env.VERCEL_ORG_ID,
+    VERCEL_PROJECT_ID: process.env.VERCEL_PROJECT_ID,
+    VERCEL_WWW_PROJECT_ID: process.env.VERCEL_WWW_PROJECT_ID,
+    VERCEL_BLOG_PROJECT_ID: process.env.VERCEL_BLOG_PROJECT_ID,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    ENV_SERVICE_TOKEN: process.env.ENV_SERVICE_TOKEN,
+    GH_SUBMODULE_SYNC_PAT: process.env.GH_SUBMODULE_SYNC_PAT,
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    NOTIFY_TOKEN: process.env.NOTIFY_TOKEN,
+    CONTAINER_DEPLOYMENT_SERVICE_TOKEN:
+      process.env.CONTAINER_DEPLOYMENT_SERVICE_TOKEN,
+    // client vars
     NEXT_PUBLIC_CURRENT_ENV: process.env.NEXT_PUBLIC_CURRENT_ENV,
     NEXT_PUBLIC_WWW_URL: process.env.NEXT_PUBLIC_WWW_URL,
     NEXT_PUBLIC_BLOG_URL: process.env.NEXT_PUBLIC_BLOG_URL,
