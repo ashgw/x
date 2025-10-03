f7333f4865dfbf5bc9aad10ddc7f004332998aad
ashgw <ashrefgw@proton.me>
2025-10-03 10:36:42 +0100

    chore: add new plugin

--- apps/www/src/express/app.ts
+++ apps/www/src/express/app.ts
@@ -1,88 +1,14 @@
 import express from "express";
-
-export const createGlobalContext = (
-  _req: express.Request,
-  _res: express.Response,
-  _next: express.NextFunction,
-): GlobalContext => {
-  _next();
-  return {
-    ctx: {
-      db,
-      requestedAt: new Date(),
-    },
-  };
-};
-
-import { createExpressEndpoints, initServer } from "@ts-rest/express";
 import bodyParser from "body-parser";
-import { db } from "@ashgw/db";
 import { logger } from "@ashgw/logger";
-import type { GlobalContext } from "~/ts-rest/context";
-
-import { initContract } from "@ts-rest/core";
-
-import { z } from "zod";
-
-const c = initContract();
-
-export const contract = c.router({
-  getPokemon: {
-    method: "GET",
-    path: "/pokemon/:id",
-    pathParams: z.object({
-      id: z.string(),
-    }),
-    responses: {
-      200: z.object({
-        name: z.string(),
-      }),
-    },
-  },
-});
+import { v1RestRouter } from "./rest";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const s = initServer();
+app.use("/v1", v1RestRouter);
 
-const router = s.router(contract, {
-  getPokemon: {
-    handler: async ({ params: { id }, req }) => {
-      logger.info("received request", {
-        ctx: {
-          ...req.app.locals.ctx,
-        },
-      });
-      await new Promise((resolve) => setTimeout(resolve, 350));
-      return {
-        status: 200,
-        body: { id, name: "Pokemon" },
-      };
-    },
-  },
-});
-
-// Mount ts-rest under /v1
-const v1Router = express.Router();
-
-createExpressEndpoints(contract, router, v1Router, {
-  responseValidation: true,
-  jsonQuery: true,
-  logInitialization: true,
-  globalMiddleware: [createGlobalContext],
-  requestValidationErrorHandler: (err, req, res, next) => {
-    res.status(400).json({
-      message: "Validation failed",
-    });
-    next();
-  },
-});
-
-app.use("/v1", v1Router);
-
-// Simple root handler
 app.get("/", (req, res) => {
   logger.info(String(req.app.locals));
   res.send("Hiii 😎🔥🚀✨");
---
+++ apps/www/src/express/rest.ts
@@ -0,0 +1,75 @@
+import express from "express";
+
+import { createExpressEndpoints, initServer } from "@ts-rest/express";
+import { db } from "@ashgw/db";
+import { logger } from "@ashgw/logger";
+import type { GlobalContext } from "~/ts-rest/context";
+
+import { initContract } from "@ts-rest/core";
+
+import { z } from "zod";
+
+const c = initContract();
+
+export const contract = c.router({
+  getPokemon: {
+    method: "GET",
+    path: "/pokemon/:id",
+    pathParams: z.object({
+      id: z.string(),
+    }),
+    responses: {
+      200: z.object({
+        name: z.string(),
+      }),
+    },
+  },
+});
+
+const s = initServer();
+
+const router = s.router(contract, {
+  getPokemon: {
+    handler: async ({ params: { id }, req }) => {
+      logger.info("received request", {
+        ctx: {
+          ...req.app.locals.ctx,
+        },
+      });
+      await new Promise((resolve) => setTimeout(resolve, 350));
+      return {
+        status: 200,
+        body: { id, name: "Pokemon" },
+      };
+    },
+  },
+});
+
+export const v1RestRouter = express.Router();
+
+export const createGlobalContext = (
+  _req: express.Request,
+  _res: express.Response,
+  _next: express.NextFunction,
+): GlobalContext => {
+  _next();
+  return {
+    ctx: {
+      db,
+      requestedAt: new Date(),
+    },
+  };
+};
+
+createExpressEndpoints(contract, router, v1RestRouter, {
+  responseValidation: true,
+  jsonQuery: true,
+  logInitialization: true,
+  globalMiddleware: [createGlobalContext],
+  requestValidationErrorHandler: (err, req, res, next) => {
+    res.status(400).json({
+      message: "Validation failed",
+    });
+    next();
+  },
+});
